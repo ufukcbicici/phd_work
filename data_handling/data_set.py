4589dce8338194825aab817ecc6651957b29c138
ufukcbicici <ufukcbicici@yahoo.com>
2017-07-03 22:34:16 +0300

    Mnist Dataset handler integration 2

--- data_handling/data_set.py
+++ data_handling/data_set.py
@@ -3,12 +3,9 @@ from auxillary.constants import DatasetTypes
 
 class DataSet:
     def __init__(self):
-        self.trainingSampleCount = None
-        self.testSampleCount = None
-        self.validationSampleCount = None
         self.dataShape = None
         self.targetShape = None
-        self.currentDataSet = DatasetTypes.training
+        self.currentDataSetType = None
         self.currentIndex = 0
 
     def load_dataset(self):
@@ -18,16 +15,16 @@ class DataSet:
         pass
 
     def reset(self):
-        self.currentIndex = 0
+        pass
+
+    def set_current_data_set_type(self, dataset_type):
+        pass
 
     def get_current_sample_count(self):
-        if self.currentDataSet == DatasetTypes.training:
-            return self.trainingSampleCount
-        elif self.currentDataSet == DatasetTypes.test:
-            return self.testSampleCount
-        elif self.currentDataSet == DatasetTypes.validation:
-            return self.validationSampleCount
-        else:
-            raise Exception("Unknown dataset type.")
+        pass
 
+    def get_label_count(self):
+        pass
 
+    def visualize_sample(self, sample_index):
+        pass
--- data_handling/mnist_data_set.py
+++ data_handling/mnist_data_set.py
@@ -1,20 +1,86 @@
 import struct
+import os
+import numpy as np
 from array import array
 from auxillary.constants import DatasetTypes
 from data_handling.data_set import DataSet
+import matplotlib.pyplot as plt
 
 
 class MnistDataSet(DataSet):
-    def __init__(self):
+    def __init__(self, validation_sample_count):
         super().__init__()
-        self.testImagesPath = "../data/t10k-images-idx3-ubyte"
-        self.testLabelsPath = "../data/t10k-labels-idx1-ubyte"
-        self.trainImagesPath = "../data/train-images-idx3-ubyte"
-        self.trainLabelsPath = "../data/train-labels-idx1-ubyte"
+        self.testImagesPath = os.path.join(os.getcwd(), "..\\data\\mnist\\t10k-images-idx3-ubyte")
+        self.testLabelsPath = os.path.join(os.getcwd(), "..\\data\\mnist\\t10k-labels-idx1-ubyte")
+        self.trainImagesPath = os.path.join(os.getcwd(), "..\\data\\mnist\\train-images-idx3-ubyte")
+        self.trainLabelsPath = os.path.join(os.getcwd(), "..\\data\\mnist\\train-labels-idx1-ubyte")
+        self.trainingSamples = None
+        self.trainingLabels = None
+        self.testSamples = None
+        self.testLabels = None
+        self.validationSamples = None
+        self.validationLabels = None
+        self.currentSamples = None
+        self.currentLabels = None
+        self.currentIndices = None
+        self.validationSampleCount = validation_sample_count
+        self.load_dataset()
+        self.set_current_data_set_type(dataset_type=DatasetTypes.training)
 
+    # PUBLIC METHODS
     def load_dataset(self):
-        train_images, train_labels = self.load(path_img=self.trainImagesPath, path_lbl=self.trainLabelsPath)
-        test_images, test_labels = self.load(path_img=self.testImagesPath, path_lbl=self.testLabelsPath)
+        self.trainingSamples, self.trainingLabels = self.load(path_img=self.trainImagesPath,
+                                                              path_lbl=self.trainLabelsPath)
+        self.testSamples, self.testLabels = self.load(path_img=self.testImagesPath, path_lbl=self.testLabelsPath)
+        random_indices = np.random.choice(self.trainingSamples.shape[0], size=self.validationSampleCount)
+        self.validationSamples = self.trainingSamples[random_indices]
+        self.validationLabels = self.trainingLabels[random_indices]
+
+    def get_next_batch(self, batch_size):
+        pass
+
+    def reset(self):
+        self.currentIndex = 0
+        indices = np.arange(self.currentSamples.shape[0])
+        np.random.shuffle(indices)
+        self.currentLabels = self.currentLabels[indices]
+        self.currentSamples = self.currentSamples[indices]
+        self.currentIndices = np.arange(self.currentSamples.shape[0])
+        np.random.shuffle(self.currentIndices)
+
+    def set_current_data_set_type(self, dataset_type):
+        self.currentDataSetType = dataset_type
+        if self.currentDataSetType == DatasetTypes.training:
+            self.currentSamples = self.trainingSamples
+            self.currentLabels = self.trainingLabels
+        elif self.currentDataSetType == DatasetTypes.test:
+            self.currentSamples = self.testSamples
+            self.currentLabels = self.testLabels
+        elif self.currentDataSetType == DatasetTypes.validation:
+            self.currentSamples = self.validationSamples
+            self.currentLabels = self.validationLabels
+        else:
+            raise Exception("Unknown dataset type")
+        self.reset()
+
+    def get_current_sample_count(self):
+        return self.currentSamples.shape[0]
+
+    def visualize_sample(self, sample_index):
+        plt.title('Label is {label}'.format(label=self.currentLabels[sample_index]))
+        plt.imshow(self.currentSamples[sample_index], cmap='gray')
+        plt.show()
+
+    def get_label_count(self):
+        label_set_count = self.testLabels.shape[0]
+        label_dict = {}
+        for i in range(0, label_set_count):
+            label = self.testLabels[i]
+            if not (label in label_dict):
+                label_dict[label] = 0
+            label_dict[label] += 1
+        label_count = len(label_dict)
+        return label_count
 
     # PRIVATE METHODS
     # Load method taken from https://github.com/sorki/python-mnist/blob/master/mnist/loader.py
@@ -42,4 +108,4 @@ class MnistDataSet(DataSet):
         for i in range(size):
             images[i][:] = image_data[i * rows * cols:(i + 1) * rows * cols]
 
-        return images, labels
+        return np.array(images), np.array(labels)
