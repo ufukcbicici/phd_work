55f6975603a6acd2dcfb24c495523a7198b59b4a
ufukcbicici <ufukcbicici@yahoo.com>
2017-11-24 21:34:40 +0300

    Confusion Analysis Improved.

--- auxillary/confusion_matrix_analyzer.py
+++ auxillary/confusion_matrix_analyzer.py
@@ -16,28 +16,47 @@ class ConfusionMatrixAnalyzer:
         # Overall leaf performance
         total_correct = np.trace(cm)
         overall_accuracy = total_correct / total
-        print("Overall accuracy:{0}".format(overall_accuracy))
+        print("Overall accuracy:{0} {1}/{2}".format(overall_accuracy, total_correct, total))
         # Determine modes
         cumulative_prob = 0.0
-        modes = []
+        modes = set()
         sorted_distribution = sorted(label_distribution_dict.items(), key=lambda tpl: tpl[1], reverse=True)
         for tpl in sorted_distribution:
             if cumulative_prob < threshold_percentile_for_modes:
-                modes.append(tpl[0])
+                modes.add(tpl[0])
                 cumulative_prob += tpl[1]
-        total_modes = np.sum(label_counts[modes])
+        total_modes_count = np.sum(label_counts[list(modes)])
         correct_modes = 0
         for mode in modes:
             correct_modes += cm[mode, mode]
-        modes_accuracy = correct_modes / total_modes
-        print("Modes accuracy:{0}".format(modes_accuracy))
+        modes_accuracy = correct_modes / total_modes_count
+        print("Modes accuracy:{0} {1}/{2}".format(modes_accuracy, correct_modes, total_modes_count))
+        # Determine non-modes
+        total_non_modes_count = total - total_modes_count
+        correct_non_modes = 0
+        for i in range(label_distribution.shape[0]):
+            if i in modes:
+                continue
+            correct_non_modes += cm[i, i]
+        non_modes_accuracy = correct_non_modes / total_non_modes_count
+        print("Non Modes accuracy:{0} {1}/{2}".format(non_modes_accuracy, correct_non_modes, total_non_modes_count))
 
 
-cm3 = DbLogger.read_confusion_matrix(run_id=797, dataset=1, iteration=60000, num_of_labels=10, leaf_id=3)
+
+print("\nLeaf 3")
+cm3 = DbLogger.read_confusion_matrix(run_id=1003, dataset=2, iteration=60000, num_of_labels=10, leaf_id=3)
 ConfusionMatrixAnalyzer.analyze_confusion_matrix(cm=cm3, threshold_percentile_for_modes=0.85)
-cm4 = DbLogger.read_confusion_matrix(run_id=797, dataset=1, iteration=60000, num_of_labels=10, leaf_id=4)
+
+print("\nLeaf 4")
+cm4 = DbLogger.read_confusion_matrix(run_id=1003, dataset=2, iteration=60000, num_of_labels=10, leaf_id=4)
 ConfusionMatrixAnalyzer.analyze_confusion_matrix(cm=cm4, threshold_percentile_for_modes=0.85)
-cm5 = DbLogger.read_confusion_matrix(run_id=797, dataset=1, iteration=60000, num_of_labels=10, leaf_id=5)
+
+print("\nLeaf 5")
+cm5 = DbLogger.read_confusion_matrix(run_id=1003, dataset=2, iteration=60000, num_of_labels=10, leaf_id=5)
 ConfusionMatrixAnalyzer.analyze_confusion_matrix(cm=cm5, threshold_percentile_for_modes=0.85)
-cm6 = DbLogger.read_confusion_matrix(run_id=797, dataset=1, iteration=60000, num_of_labels=10, leaf_id=6)
+
+print("\nLeaf 6")
+cm6 = DbLogger.read_confusion_matrix(run_id=1003, dataset=2, iteration=60000, num_of_labels=10, leaf_id=6)
 ConfusionMatrixAnalyzer.analyze_confusion_matrix(cm=cm6, threshold_percentile_for_modes=0.85)
+
+print("X")
