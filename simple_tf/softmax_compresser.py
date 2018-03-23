1aa86cd82625a8215c4cdc4ff03067fbe00c0e39
ufukcbicici <ufukcbicici@yahoo.com>
2018-03-23 17:32:54 +0300

    Separate info gain balances for each node

--- simple_tf/softmax_compresser.py
+++ simple_tf/softmax_compresser.py
@@ -9,15 +9,14 @@ class SoftmaxCompresser:
     @staticmethod
     def compress_network_softmax(network, sess, dataset):
         # Get all final feature vectors for all leaves, for the complete training set.
-        featureVectorsDict = {}
+        posteriorsDict = {}
         dataset.set_current_data_set_type(dataset_type=DatasetTypes.training)
         while True:
             results = network.eval_network(sess=sess, dataset=dataset, use_masking=True)
             for leafNode in network.leafNodes:
-                final_feature_ref = network.get_variable_name(name="final_eval_feature", node=leafNode)
-                final_leaf_features = results[final_feature_ref]
-                UtilityFuncs.concat_to_np_array_dict(dct=featureVectorsDict, key=leafNode.index,
-                                                     array=final_leaf_features)
+                posterior_ref = network.get_variable_name(name="posterior_probs", node=leafNode)
+                posterior_probs = results[posterior_ref]
+                UtilityFuncs.concat_to_np_array_dict(dct=posteriorsDict, key=leafNode.index, array=posterior_probs)
             if dataset.isNewEpoch:
                 break
         print("X")
