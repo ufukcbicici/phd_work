7d046ec98f0c457ec34950d7420cdf1237f72176
ufukcbicici <ufukcbicici@yahoo.com>
2018-03-28 01:28:23 +0300

    Global info gain balance coefficient is back.

--- algorithms/mode_tracker.py
+++ algorithms/mode_tracker.py
@@ -7,10 +7,19 @@ from simple_tf.global_params import GlobalConstants
 class ModeTracker:
     def __init__(self, network):
         self.network = network
+        self.unchangedEpochCount = 0
         self.modesHistory = []
 
     def get_modes(self):
         return self.modesHistory[-1]
+
+    def modes_changed(self, prev_modes, curr_modes):
+        for node in self.network.topologicalSortedNodes:
+            if not node.isLeaf:
+                continue
+            if prev_modes[node.index] != curr_modes[node.index]:
+                return True
+        return False
 
     def calculate_modes(self, leaf_true_labels_dict, dataset, dataset_type, kv_rows, run_id, iteration):
         # Measure overall label distribution in leaves, get modes
@@ -51,3 +60,19 @@ class ModeTracker:
         if dataset_type == DatasetTypes.training:
             kv_rows.append((run_id, iteration, "Total Mode Count", total_mode_count))
         self.modesHistory.append(modes_per_leaves)
+
+    def check_for_compression_start(self, dataset):
+        label_count = dataset.get_label_count()
+        total_mode_count = 0
+        # for modes in last_modes.values():
+        #     total_mode_count += len(modes)
+        if len(self.modesHistory) == 0:
+            raise Exception("Mode history can't be zero.")
+        elif len(self.modesHistory) == 1:
+            self.unchangedEpochCount = 1
+        else:
+            prev_modes = self.modesHistory[-2]
+            curr_modes = self.modesHistory[-1]
+
+
+
