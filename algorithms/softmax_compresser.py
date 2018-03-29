c110a5d6bdc34355dc94053610cb1e689c4dc7cf
t67rtyus <ufukcbicici@yahoo.com>
2018-03-29 16:52:23 +0300

    Writing the distillation network code.

--- algorithms/softmax_compresser.py
+++ algorithms/softmax_compresser.py
@@ -13,12 +13,12 @@ class SoftmaxCompresser:
     @staticmethod
     def compress_network_softmax(network, sess, dataset):
         # Get all final feature vectors for all leaves, for the complete training set.
-        featureVectorsDict = {}
-        logitsDict = {}
-        posteriorsDict = {}
-        oneHotLabelsDict = {}
-        softmaxWeights = {}
-        softmaxBiases = {}
+        feature_vectors_dict = {}
+        logits_dict = {}
+        posteriors_dict = {}
+        one_hot_labels_dict = {}
+        softmax_weights = {}
+        softmax_biases = {}
         dataset.set_current_data_set_type(dataset_type=DatasetTypes.training)
         while True:
             results = network.eval_network(sess=sess, dataset=dataset, use_masking=True)
@@ -28,23 +28,23 @@ class SoftmaxCompresser:
                 leaf_node = node
                 posterior_ref = network.get_variable_name(name="posterior_probs", node=leaf_node)
                 posterior_probs = results[posterior_ref]
-                UtilityFuncs.concat_to_np_array_dict(dct=posteriorsDict, key=leaf_node.index, array=posterior_probs)
+                UtilityFuncs.concat_to_np_array_dict(dct=posteriors_dict, key=leaf_node.index, array=posterior_probs)
                 final_feature_ref = network.get_variable_name(name="final_eval_feature", node=leaf_node)
                 final_leaf_features = results[final_feature_ref]
-                UtilityFuncs.concat_to_np_array_dict(dct=featureVectorsDict, key=leaf_node.index,
+                UtilityFuncs.concat_to_np_array_dict(dct=feature_vectors_dict, key=leaf_node.index,
                                                      array=final_leaf_features)
                 one_hot_label_ref = "Node{0}_one_hot_label_tensor".format(leaf_node.index)
                 one_hot_labels = results[one_hot_label_ref]
-                UtilityFuncs.concat_to_np_array_dict(dct=oneHotLabelsDict, key=leaf_node.index,
+                UtilityFuncs.concat_to_np_array_dict(dct=one_hot_labels_dict, key=leaf_node.index,
                                                      array=one_hot_labels)
                 logits_ref = network.get_variable_name(name="logits", node=leaf_node)
                 logits = results[logits_ref]
-                UtilityFuncs.concat_to_np_array_dict(dct=logitsDict, key=leaf_node.index,
+                UtilityFuncs.concat_to_np_array_dict(dct=logits_dict, key=leaf_node.index,
                                                      array=logits)
                 softmax_weights_ref = network.get_variable_name(name="fc_softmax_weights", node=leaf_node)
-                softmaxWeights[leaf_node.index] = results[softmax_weights_ref]
+                softmax_weights[leaf_node.index] = results[softmax_weights_ref]
                 softmax_biases_ref = network.get_variable_name(name="fc_softmax_biases", node=leaf_node)
-                softmaxBiases[leaf_node.index] = results[softmax_biases_ref]
+                softmax_biases[leaf_node.index] = results[softmax_biases_ref]
 
             if dataset.isNewEpoch:
                 break
@@ -53,12 +53,12 @@ class SoftmaxCompresser:
             if not node.isLeaf:
                 continue
             leaf_node = node
-            softmax_weight = softmaxWeights[leaf_node.index]
-            softmax_bias = softmaxBiases[leaf_node.index]
-            feature_vectors = featureVectorsDict[leaf_node.index]
-            logits = logitsDict[leaf_node.index]
-            probs = posteriorsDict[leaf_node.index]
-            one_hot_labels = oneHotLabelsDict[leaf_node.index]
+            softmax_weight = softmax_weights[leaf_node.index]
+            softmax_bias = softmax_biases[leaf_node.index]
+            feature_vectors = feature_vectors_dict[leaf_node.index]
+            logits = logits_dict[leaf_node.index]
+            probs = posteriors_dict[leaf_node.index]
+            one_hot_labels = one_hot_labels_dict[leaf_node.index]
             # Unit Test
             SoftmaxCompresser.assert_prob_correctness(softmax_weights=softmax_weight, softmax_biases=softmax_bias,
                                                       features=feature_vectors, logits=logits, probs=probs)
@@ -107,8 +107,8 @@ class SoftmaxCompresser:
         soft_labels_cost_weight = tf.placeholder(tf.float32)
         hard_labels_cost_weight = tf.placeholder(tf.float32)
         l2_loss_weight = tf.placeholder(tf.float32)
-        modesPerLeaves = network.modeTracker.get_modes()
-        compressed_class_count = len(modesPerLeaves[leaf_node.index]) + 1
+        modes_per_leaves = network.modeTracker.get_modes()
+        compressed_class_count = len(modes_per_leaves[leaf_node.index]) + 1
         # Get new class count: Mode labels + Outliers. Init the new classifier hyperplanes.
         softmax_weights = tf.Variable(
             tf.truncated_normal([features_dim, compressed_class_count],
@@ -120,13 +120,13 @@ class SoftmaxCompresser:
             tf.constant(0.1, shape=[compressed_class_count], dtype=GlobalConstants.DATA_TYPE),
             name=network.get_variable_name(name="distilled_fc_softmax_biases", node=leaf_node))
         # Compressed softmax probabilities
-        logits = tf.matmul(features_tensor, softmax_weights) + softmax_biases
+        compressed_logits = tf.matmul(features_tensor, softmax_weights) + softmax_biases
         # Prepare the loss function, according to Hinton's Distillation Recipe
         # Term 1: Cross entropy between the tempered, squashed posteriors p and q: H(p,q)
-        soft_loss_vec = tf.nn.softmax_cross_entropy_with_logits(labels=p, logits=logits)
+        soft_loss_vec = tf.nn.softmax_cross_entropy_with_logits(labels=p, logits=compressed_logits)
         soft_loss = soft_labels_cost_weight * tf.reduce_mean(soft_loss_vec)
         # Term 2: Cross entropy between the hard labels and q
-        hard_loss_vec = tf.nn.softmax_cross_entropy_with_logits(labels=t, logits=logits)
+        hard_loss_vec = tf.nn.softmax_cross_entropy_with_logits(labels=t, logits=compressed_logits)
         hard_loss = hard_labels_cost_weight * tf.reduce_mean(hard_loss_vec)
         # Term 3: L2 loss for softmax weights
         weight_l2 = l2_loss_weight * tf.nn.l2_loss(softmax_weights)
@@ -173,10 +173,11 @@ class SoftmaxCompresser:
         # Order mode labels from small to large, assign the smallest label to "0", next one to "1" and so on.
         # If there are N modes, Outlier class will have N as the label.
         label_count = one_hot_labels.shape[1]
-        sorted_modes = sorted(network.modesPerLeaves[leaf_node.index])
-        non_mode_labels = [l for l in range(label_count) if l not in network.modesPerLeaves[leaf_node.index]]
+        modes_per_leaves = network.modeTracker.get_modes()
+        sorted_modes = sorted(modes_per_leaves[leaf_node.index])
+        non_mode_labels = [l for l in range(label_count) if l not in modes_per_leaves[leaf_node.index]]
         mode_posteriors = posteriors[:, sorted_modes]
-        outlier_posteriors = np.sum(posteriors[:, non_mode_labels], 1)
+        outlier_posteriors = np.sum(posteriors[:, non_mode_labels], 1).reshape(posteriors.shape[0], 1)
         compressed_posteriors = np.concatenate((mode_posteriors, outlier_posteriors), axis=1)
         mode_one_hot_entries = one_hot_labels[:, sorted_modes]
         outlier_one_hot_entries = np.sum(one_hot_labels[:, non_mode_labels], 1)
