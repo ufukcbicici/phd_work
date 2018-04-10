75b62e42623f8d19fb42dca0e78273ce1afbd555
t67rtyus <ufukcbicici@yahoo.com>
2018-04-10 07:30:14 +0300

    Distillation Cross Validation Code.

--- algorithms/softmax_compresser.py
+++ algorithms/softmax_compresser.py
@@ -161,12 +161,10 @@ class SoftmaxCompresser:
         global_step = tf.Variable(name="global_step", initial_value=0, trainable=False)
         # Train by cross-validation
         temperature_list = [1.0]
-        soft_loss_weights = [0.0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45,
-                             0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0]
+        soft_loss_weights = [0.0, 0.25, 0.5, 0.75, 1.0]
         hard_loss_weights = [1.0]
-        l2_weights = [0.0]
-        learning_rates = [0.001, 0.005, 0.01, 0.015, 0.02, 0.025, 0.03, 0.035, 0.04, 0.045, 0.05,
-                          0.055, 0.06, 0.065, 0.07, 0.075, 0.08, 0.085, 0.09, 0.095, 0.1]
+        l2_weights = [0.0, 0.0001, 0.00025, 0.0005, 0.00075, 0.001]
+        learning_rates = [0.001, 0.005, 0.025, 0.05, 0.075, 0.1]
         cross_validation_repeat_count = 10
         cartesian_product = UtilityFuncs.get_cartesian_product(list_of_lists=[learning_rates,
                                                                               temperature_list, soft_loss_weights,
