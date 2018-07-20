Fixed Class Mode Computation Strategy

algorithms/mode_tracker.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from auxillary.constants import DatasetTypes
-from simple_tf.global_params import GlobalConstants, ModeTrackingStrategy
+from simple_tf.global_params import GlobalConstants, ModeTrackingStrategy, ModeComputationStrategy
 
 
 class ModeTracker:
@@ -28,9 +28,10 @@ class ModeTracker:
         return False
 
     def calculate_modes(self, leaf_true_labels_dict, dataset, dataset_type, kv_rows, run_id, iteration):
-        # Measure overall label distribution in leaves, get modes
+        # Measure overall label distribution in leaves
         total_mode_count = 0
         modes_per_leaves = {}
+        leaf_label_distributions = {}
         for node in self.network.topologicalSortedNodes:
             if not node.isLeaf:
                 continue
@@ -45,27 +46,46 @@ class ModeTracker:
                 frequencies[label] = np.sum(true_labels == label)
                 label_distribution[label] = frequencies[label] / float(total_sample_count)
                 distribution_str += "{0}:{1:.3f} ".format(label, label_distribution[label])
-            # Get modes
+                print("Node{0} Label Distribution: {1}".format(node.index, distribution_str))
+            leaf_label_distributions[node.index] = label_distribution
+        if GlobalConstants.MODE_COMPUTATION_STRATEGY == ModeComputationStrategy.percentile:
             if dataset_type == DatasetTypes.training:
-                cumulative_prob = 0.0
-                sorted_distribution = sorted(label_distribution.items(), key=lambda lbl: lbl[1], reverse=True)
-                modes_per_leaves[node.index] = set()
-                mode_txt = ""
-                for tpl in sorted_distribution:
-                    if cumulative_prob < GlobalConstants.PERCENTILE_THRESHOLD:
+                for node in self.network.topologicalSortedNodes:
+                    if not node.isLeaf:
+                        continue
+                    label_distribution = leaf_label_distributions[node.index]
+                    cumulative_prob = 0.0
+                    sorted_distribution = sorted(label_distribution.items(), key=lambda lbl: lbl[1], reverse=True)
+                    modes_per_leaves[node.index] = set()
+                    mode_txt = ""
+                    for tpl in sorted_distribution:
+                        if cumulative_prob < GlobalConstants.PERCENTILE_THRESHOLD:
+                            modes_per_leaves[node.index].add(tpl[0])
+                            mode_txt += str(tpl[0]) + ","
+                            cumulative_prob += tpl[1]
+                    mode_txt = mode_txt[0:len(mode_txt) - 1]
+                    total_mode_count += len(modes_per_leaves[node.index])
+                    kv_rows.append((run_id, iteration, "Leaf {0} Modes".format(node.index), mode_txt))
+                    kv_rows.append((run_id, iteration, "Total Mode Count", total_mode_count))
+                    self.modesHistory.append(modes_per_leaves)
+        elif GlobalConstants.MODE_COMPUTATION_STRATEGY == ModeComputationStrategy.max_num_of_classes:
+            if dataset_type == DatasetTypes.training:
+                for node in self.network.topologicalSortedNodes:
+                    if not node.isLeaf:
+                        continue
+                    label_distribution = leaf_label_distributions[node.index]
+                    sorted_distribution = sorted(label_distribution.items(), key=lambda lbl: lbl[1], reverse=True)
+                    modes_per_leaves[node.index] = set()
+                    mode_txt = ""
+                    for i in range(GlobalConstants.MAX_MODE_CLASSES):
+                        tpl = sorted_distribution[i]
                         modes_per_leaves[node.index].add(tpl[0])
                         mode_txt += str(tpl[0]) + ","
-                        cumulative_prob += tpl[1]
-                mode_txt = mode_txt[0:len(mode_txt) - 1]
-                total_mode_count += len(modes_per_leaves[node.index])
-                kv_rows.append((run_id, iteration, "Leaf {0} Modes".format(node.index), mode_txt))
-            print("Node{0} Label Distribution: {1}".format(node.index, distribution_str))
-        # if dataset_type == DatasetTypes.training and total_mode_count != GlobalConstants.NUM_LABELS:
-        #     raise Exception("total_mode_count != GlobalConstants.NUM_LABELS")
-        # Measure overall information gain
-        if dataset_type == DatasetTypes.training:
-            kv_rows.append((run_id, iteration, "Total Mode Count", total_mode_count))
-            self.modesHistory.append(modes_per_leaves)
+                    mode_txt = mode_txt[0:len(mode_txt) - 1]
+                    total_mode_count += len(modes_per_leaves[node.index])
+                    kv_rows.append((run_id, iteration, "Leaf {0} Modes".format(node.index), mode_txt))
+                    kv_rows.append((run_id, iteration, "Total Mode Count", total_mode_count))
+                    self.modesHistory.append(modes_per_leaves)
 
     def check_for_compression_start(self, dataset, epoch):
         if GlobalConstants.MODE_TRACKING_STRATEGY == ModeTrackingStrategy.wait_for_convergence: