Fixed Class Mode Computation Strategy

simple_tf/entry.py
@@ -36,7 +36,7 @@ def get_explanation_string(network):
         total_param_count += np.prod(v.get_shape().as_list())
 
     # Tree
-    explanation = "SVM - Fashion Mnist - Connected H - Tests - Parallel Dnns, Softmax Distillation 16 H Fixed Class\n"
+    explanation = "SVM - Fashion Mnist - Connected H - Tests - Parallel Dnns, Softmax Distillation 16 H Fast Tree DGX v1\n"
     # "(Lr=0.01, - Decay 1/(1 + i*0.0001) at each i. iteration)\n"
     explanation += "Using Fast Tree Version:{0}\n".format(GlobalConstants.USE_FAST_TREE_MODE)
     explanation += "Batch Size:{0}\n".format(GlobalConstants.BATCH_SIZE)
@@ -49,7 +49,9 @@ def get_explanation_string(network):
     explanation += "********Lr Settings********\n"
     explanation += GlobalConstants.LEARNING_RATE_CALCULATOR.get_explanation()
     explanation += "********Lr Settings********\n"
+    explanation += "********Decision Loss Weight Settings********\n"
     explanation += network.decisionLossCoefficientCalculator.get_explanation()
+    explanation += "********Decision Loss Weight Settings********\n"
     explanation += "Param Count:{0}\n".format(total_param_count)
     explanation += "Wd:{0}\n".format(GlobalConstants.WEIGHT_DECAY_COEFFICIENT)
     explanation += "Decision Wd:{0}\n".format(GlobalConstants.DECISION_WEIGHT_DECAY_COEFFICIENT)
@@ -443,18 +445,12 @@ def main_fast_tree():
     decision_wd = [0.0]
     info_gain_balance_coeffs = [5.0]
     classification_dropout_probs = \
-        [0.15, 0.15, 0.15, 0.15, 0.15, 0.15,
-         0.15, 0.15, 0.15, 0.15, 0.15, 0.15,
-         0.15, 0.15, 0.15, 0.15, 0.15, 0.15,
-         0.2, 0.2, 0.2, 0.2, 0.2, 0.2,
-         0.2, 0.2, 0.2, 0.2, 0.2, 0.2,
-         0.2, 0.2, 0.2, 0.2, 0.2, 0.2,
-         0.25, 0.25, 0.25, 0.25, 0.25, 0.25,
-         0.25, 0.25, 0.25, 0.25, 0.25, 0.25,
-         0.3, 0.3, 0.3, 0.3, 0.3, 0.3,
-         0.3, 0.3, 0.3, 0.3, 0.3, 0.3,
-         0.3, 0.3, 0.3, 0.3, 0.3, 0.3,
-         0.3, 0.3, 0.3, 0.3, 0.3, 0.3
+        [0.1, 0.1, 0.1, 0.1, 0.1, 0.1,
+         0.1, 0.1, 0.1, 0.1, 0.1, 0.1,
+         0.1, 0.1, 0.1, 0.1, 0.1, 0.1,
+         0.05, 0.05, 0.05, 0.05, 0.05, 0.05,
+         0.05, 0.05, 0.05, 0.05, 0.05, 0.05,
+         0.05, 0.05, 0.05, 0.05, 0.05, 0.05
          ]
     decision_dropout_probs = [0.35]
     # # [0.0, 0.0, 0.0, 0.0, 0.0, 0.0,