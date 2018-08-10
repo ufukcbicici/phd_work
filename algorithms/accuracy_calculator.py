Fixed Class Mode Computation Strategy

algorithms/accuracy_calculator.py
@@ -1,3 +1,5 @@
+import threading
+
 import numpy as np
 
 from auxillary.constants import DatasetTypes
@@ -5,6 +7,89 @@ from auxillary.db_logger import DbLogger
 from auxillary.general_utility_funcs import UtilityFuncs
 from simple_tf.global_params import GlobalConstants
 from collections import deque
+
+
+class MultipathCalculator(threading.Thread):
+    def __init__(self, thread_id, run_id, iteration, threshold_list,
+                 network, sample_count, label_dict, branch_probs, posterior_probs):
+        threading.Thread.__init__(self)
+        self.threadId = thread_id
+        self.runId = run_id
+        self.iteration = iteration
+        self.thresholdList = threshold_list
+        self.network = network
+        self.sampleCount = sample_count
+        self.labelDict = label_dict
+        self.branchProbs = branch_probs
+        self.posteriorProbs = posterior_probs
+        self.kvRows = []
+
+    def run(self):
+        root_node = self.network.nodes[0]
+        for path_threshold in self.thresholdList:
+            total_correct_simple_avg = 0
+            total_correct_weighted_avg = 0
+            total_leaves_evaluated = 0
+            for sample_index in range(self.sampleCount):
+                true_label = self.labelDict[sample_index]
+                queue = deque([(root_node, 1.0)])
+                leaf_path_probs = {}
+                leaf_posteriors = {}
+                while len(queue) > 0:
+                    pair = queue.popleft()
+                    curr_node = pair[0]
+                    path_probability = pair[1]
+                    if not curr_node.isLeaf:
+                        p_n_given_sample = self.branchProbs[curr_node.index][sample_index, :]
+                        child_nodes = self.network.dagObject.children(node=curr_node)
+                        child_nodes_sorted = sorted(child_nodes, key=lambda c_node: c_node.index)
+                        for index in range(len(child_nodes_sorted)):
+                            if p_n_given_sample[index] >= path_threshold:
+                                queue.append((child_nodes_sorted[index], path_probability * p_n_given_sample[index]))
+                    else:
+                        sample_posterior = self.posteriorProbs[curr_node.index][sample_index, :]
+                        assert curr_node.index not in leaf_path_probs and curr_node.index not in leaf_posteriors
+                        leaf_path_probs[curr_node.index] = path_probability
+                        leaf_posteriors[curr_node.index] = sample_posterior
+                assert len(leaf_path_probs) > 0 and \
+                       len(leaf_posteriors) > 0 and \
+                       len(leaf_path_probs) == len(leaf_posteriors)
+                total_leaves_evaluated += len(leaf_posteriors)
+                # Method 1: Simply take the average of all posteriors
+                final_posterior = None
+                # posterior_matrix = np.concatenate(list(leaf_posteriors.values()), axis=0)
+                # final_posterior = np.mean(posterior_matrix, axis=0)
+                # prediction_simple_avg = np.argmax(final_posterior)
+                for posterior in leaf_posteriors.values():
+                    if final_posterior is None:
+                        final_posterior = np.copy(posterior)
+                    else:
+                        final_posterior += posterior
+                final_posterior = (1.0 / len(leaf_posteriors)) * final_posterior
+                prediction_simple_avg = np.argmax(final_posterior)
+                # Method 2: Weighted average of all posteriors
+                final_posterior_weighted = np.copy(final_posterior)
+                final_posterior_weighted.fill(0.0)
+                normalizing_constant = 0
+                for k, v in leaf_posteriors.items():
+                    normalizing_constant += leaf_path_probs[k]
+                    final_posterior_weighted += leaf_path_probs[k] * v
+                final_posterior_weighted = (1.0 / normalizing_constant) * final_posterior_weighted
+                prediction_weighted_avg = np.argmax(final_posterior_weighted)
+                if prediction_simple_avg == true_label:
+                    total_correct_simple_avg += 1
+                if prediction_weighted_avg == true_label:
+                    total_correct_weighted_avg += 1
+            accuracy_simple_avg = float(total_correct_simple_avg) / float(self.sampleCount)
+            accuracy_weighted_avg = float(total_correct_weighted_avg) / float(self.sampleCount)
+            print(
+                "******* Multipath Threshold:{0} Simple Accuracy:{1} "
+                "Weighted Accuracy:{2} Total Leaves Evaluated:{3}*******"
+                    .format(path_threshold, accuracy_simple_avg, accuracy_weighted_avg, total_leaves_evaluated))
+            self.kvRows.append((self.runId, self.iteration, 0, path_threshold, accuracy_simple_avg,
+                                total_leaves_evaluated))
+            self.kvRows.append((self.runId, self.iteration, 1, path_threshold, accuracy_weighted_avg,
+                                total_leaves_evaluated))
 
 
 class AccuracyCalculator:
@@ -375,7 +460,7 @@ class AccuracyCalculator:
                                          leaf_true_labels_dict=leaf_true_labels_dict,
                                          dataset=dataset, dataset_type=dataset_type)
 
-    def calculate_accuracy_multipath(self, sess, dataset, dataset_type):
+    def calculate_accuracy_multipath(self, sess, dataset, dataset_type, run_id, iteration):
         dataset.set_current_data_set_type(dataset_type=dataset_type)
         leaf_predicted_labels_dict = {}
         leaf_true_labels_dict = {}
@@ -411,66 +496,22 @@ class AccuracyCalculator:
         wrong_samples_with_non_mode_predictions = set()
         true_labels_dict = {}
         modes_per_leaves = self.network.modeTracker.get_modes()
-        for path_threshold in GlobalConstants.MULTIPATH_SCHEDULES:
-            total_correct_simple_avg = 0
-            total_correct_weighted_avg = 0
-            total_leaves_evaluated = 0
-            for sample_index in range(sample_count):
-                true_label = label_dict[sample_index]
-                queue = deque([(root_node, 1.0)])
-                leaf_path_probs = {}
-                leaf_posteriors = {}
-                while len(queue) > 0:
-                    pair = queue.popleft()
-                    curr_node = pair[0]
-                    path_probability = pair[1]
-                    if not curr_node.isLeaf:
-                        p_n_given_sample = branch_probs[curr_node.index][sample_index, :]
-                        child_nodes = self.network.dagObject.children(node=curr_node)
-                        child_nodes_sorted = sorted(child_nodes, key=lambda c_node: c_node.index)
-                        for index in range(len(child_nodes_sorted)):
-                            if p_n_given_sample[index] >= path_threshold:
-                                queue.append((child_nodes_sorted[index], path_probability * p_n_given_sample[index]))
-                    else:
-                        sample_posterior = posterior_probs[curr_node.index][sample_index, :]
-                        assert curr_node.index not in leaf_path_probs and curr_node.index not in leaf_posteriors
-                        leaf_path_probs[curr_node.index] = path_probability
-                        leaf_posteriors[curr_node.index] = sample_posterior
-                assert len(leaf_path_probs) > 0 and \
-                       len(leaf_posteriors) > 0 and \
-                       len(leaf_path_probs) == len(leaf_posteriors)
-                total_leaves_evaluated += len(leaf_posteriors)
-                # Method 1: Simply take the average of all posteriors
-                final_posterior = None
-                # posterior_matrix = np.concatenate(list(leaf_posteriors.values()), axis=0)
-                # final_posterior = np.mean(posterior_matrix, axis=0)
-                # prediction_simple_avg = np.argmax(final_posterior)
-                for posterior in leaf_posteriors.values():
-                    if final_posterior is None:
-                        final_posterior = np.copy(posterior)
-                    else:
-                        final_posterior += posterior
-                final_posterior = (1.0 / len(leaf_posteriors)) * final_posterior
-                prediction_simple_avg = np.argmax(final_posterior)
-                # Method 2: Weighted average of all posteriors
-                final_posterior_weighted = np.copy(final_posterior)
-                final_posterior_weighted.fill(0.0)
-                normalizing_constant = 0
-                for k,v in leaf_posteriors.items():
-                    normalizing_constant += leaf_path_probs[k]
-                    final_posterior_weighted += leaf_path_probs[k]*v
-                final_posterior_weighted = (1.0 / normalizing_constant) * final_posterior_weighted
-                prediction_weighted_avg = np.argmax(final_posterior_weighted)
-                if prediction_simple_avg == true_label:
-                    total_correct_simple_avg += 1
-                if prediction_weighted_avg == true_label:
-                    total_correct_weighted_avg += 1
-            accuracy_simple_avg = float(total_correct_simple_avg) / float(sample_count)
-            accuracy_weighted_avg = float(total_correct_weighted_avg) / float(sample_count)
-            print(
-                "******* Multipath Threshold:{0} Simple Accuracy:{1} "
-                "Weighted Accuracy:{2} Total Leaves Evaluated:{3}*******"
-                .format(path_threshold, accuracy_simple_avg, accuracy_weighted_avg, total_leaves_evaluated))
+        threshold_dict = UtilityFuncs.distribute_evenly_to_threads(
+            num_of_threads=GlobalConstants.SOFTMAX_DISTILLATION_CPU_COUNT,
+            list_to_distribute=GlobalConstants.MULTIPATH_SCHEDULES)
+        threads_dict = {}
+        for thread_id in range(GlobalConstants.SOFTMAX_DISTILLATION_CPU_COUNT):
+            threads_dict[thread_id] = MultipathCalculator(thread_id=thread_id, run_id=run_id, iteration=iteration,
+                                                          threshold_list=threshold_dict[thread_id], network=self.network,
+                                                          sample_count=sample_count, label_dict=label_dict,
+                                                          branch_probs=branch_probs, posterior_probs=posterior_probs)
+            threads_dict[thread_id].start()
+        all_results = []
+        for thread in threads_dict.values():
+            thread.join()
+        for thread in threads_dict.values():
+            all_results.extend(thread.kvRows)
+        DbLogger.write_into_table(rows=all_results, table=DbLogger.multipath_results_table, col_count=6)
 
     def calculate_accuracy_with_route_correction(self, sess, dataset, dataset_type):
         dataset.set_current_data_set_type(dataset_type=dataset_type)

simple_tf/fashion_net_decision_connected_to_f.py
@@ -217,8 +217,6 @@ def residue_network_func(network):
     variable_list = []
     curr_layer = network.residueInputTensor
     for layer_index in range(GlobalConstants.FASHION_F_RESIDUE_LAYER_COUNT):
-        if GlobalConstants.FASHION_F_RESIDUE_USE_DROPOUT:
-            curr_layer = tf.nn.dropout(curr_layer, keep_prob=network.classificationDropoutKeepProb)
         input_dim = curr_layer.get_shape().as_list()[-1]
         fc_residue_weights = tf.Variable(
             tf.truncated_normal([input_dim, GlobalConstants.FASHION_F_RESIDUE], stddev=0.1, seed=GlobalConstants.SEED,
@@ -228,6 +226,8 @@ def residue_network_func(network):
                                       name="fc_residue_bias_{0}".format(layer_index))
         variable_list.extend([fc_residue_weights, fc_residue_bias])
         curr_layer = tf.nn.relu(tf.matmul(curr_layer, fc_residue_weights) + fc_residue_bias)
+        if GlobalConstants.FASHION_F_RESIDUE_USE_DROPOUT:
+            curr_layer = tf.nn.dropout(curr_layer, keep_prob=network.classificationDropoutKeepProb)
     # Loss layer
     input_dim = curr_layer.get_shape().as_list()[-1]
     fc_residue_softmax_weights = tf.Variable(