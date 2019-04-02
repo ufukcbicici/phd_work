537e75469f2720a90b4b3897109c6a9634afb23d
ufukcbicici <ufukcbicici@yahoo.com>
2019-04-02 12:04:47 +0300

    CIGN with Sampling added.

--- simple_tf/cifar_nets/cifar_entry.py
+++ simple_tf/cifar_nets/cifar_entry.py
@@ -20,7 +20,7 @@ def get_explanation_string(network):
         total_param_count += np.prod(v.get_shape().as_list())
 
     # Tree
-    explanation = "Resnet-50 CIGN Tests\n"
+    explanation = "Resnet-50 Sampling CIGN Tests\n"
     # "(Lr=0.01, - Decay 1/(1 + i*0.0001) at each i. iteration)\n"
     explanation += "Using Fast Tree Version:{0}\n".format(GlobalConstants.USE_FAST_TREE_MODE)
     explanation += "Batch Size:{0}\n".format(GlobalConstants.BATCH_SIZE)
