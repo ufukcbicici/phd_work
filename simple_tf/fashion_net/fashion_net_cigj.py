350c253b55035ad3d846f5a1c9099daa1510c0fb
ufuk.bicici <ufukcbicici@yahoo.com>
2019-04-03 16:47:17 +0300

    Routing Test implementation.

--- simple_tf/fashion_net/fashion_net_cigj.py
+++ simple_tf/fashion_net/fashion_net_cigj.py
@@ -66,6 +66,7 @@ class FashionNetCigj:
 
     @staticmethod
     def f_conv_layer_func(node, network):
+        network.mask_input_nodes(node=node)
         filter_size = GlobalConstants.CIGJ_FASHION_NET_CONV_FILTER_SIZES[node.depth]
         num_of_input_channels = 1 if node.depth == 0 else GlobalConstants.CIGJ_FASHION_NET_OUTPUT_DIMS[node.depth-1]
         num_of_output_channels = GlobalConstants.CIGJ_FASHION_NET_OUTPUT_DIMS[node.depth]
@@ -77,6 +78,7 @@ class FashionNetCigj:
 
     @staticmethod
     def f_fc_layer_func(node, network):
+        network.mask_input_nodes(node=node)
         net = tf.contrib.layers.flatten(node.F_input)
         # net = UtilityFuncs.tf_safe_flatten(input_tensor=node.F_input)
         flattened_F_feature_size = net.get_shape().as_list()[-1]
@@ -92,6 +94,7 @@ class FashionNetCigj:
 
     @staticmethod
     def f_leaf_func(node, network):
+        network.mask_input_nodes(node=node)
         final_feature = node.F_input
         network.apply_loss_jungle(node=node, final_feature=final_feature)
 
