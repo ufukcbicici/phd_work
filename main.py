bd30ef6bfbd0b289a779dd437a806d50a770db10
ufukcbicici <ufukcbicici@yahoo.com>
2019-05-03 15:08:06 +0300

    Stride correction

--- main.py
+++ main.py
@@ -14,11 +14,14 @@ import simple_tf.cifar_nets.cifar_entry as cifar_entry
 import tensorflow as tf
 
 from simple_tf.cigj import cigj_full_training_entry, cigj_approx_training_entry
+from tf_experiments.multi_gpu_experiments import multi_gpu
 
 if __name__ == "__main__":
     print("Main - Hello World. With Import.")
     print(tf.__version__)
     # cifar_entry.cifar100_training()
-    cigj_full_training_entry.cigj_training()
+    # cigj_full_training_entry.cigj_training()
+    multi_gpu.experiment()
+
 
 
--- tf_experiments/multi_gpu_experiments/multi_gpu.py
+++ tf_experiments/multi_gpu_experiments/multi_gpu.py
@@ -5,15 +5,34 @@ from tensorflow.contrib.nccl.ops import gen_nccl_ops
 
 from auxillary.general_utility_funcs import UtilityFuncs
 
-available_devices = UtilityFuncs.get_available_devices()
-print(available_devices)
-batch_size = 250
-input_tensors = []
-for device_id, device in enumerate(available_devices):
-    input_tensor = tf.placeholder(name="input_{0}".format(device_id), dtype=tf.float32, shape=(batch_size, 32, 32, 20))
-    input_tensors.append(input_tensor)
+def experiment():
+    available_devices = UtilityFuncs.get_available_devices()
+    print(available_devices)
+    batch_size = 250
+    gpu_count = 1
+    input_tensors = []
+    means = []
+    with tf.device("/cpu:0"):
+        with tf.variable_scope("Shared"):
+            for device_id in range(gpu_count):
+                with tf.device("/gpu:{0}".format(device_id)):
+                    # Input Tensors
+                    input_tensor = tf.placeholder(name="input_{0}".format(device_id),
+                                                  dtype=tf.float32, shape=(batch_size, 32, 32, 20))
+                    input_tensors.append(input_tensor)
+                    # Mean
+                    mean = tf.reduce_mean(input_tensor, axis=[0, 1, 2])
+                    means.append(mean)
+    input_tensors_numpy = \
+        [np.random.uniform(0, 1.0, (batch_size, 32, 32, 20)) for _ in range(gpu_count)]
+    sess = tf.Session()
+    res = sess.run([means],
+                   feed_dict={input_tensors[device_id]: input_arr
+                              for device_id, input_arr in enumerate(input_tensors_numpy)})
+    print("X")
 
-input_tensors_numpy = [np.random.uniform(0, 1.0, (batch_size, 32, 32, 20)) for _ in range(len(available_devices))]
-# gen_nccl_ops.nccl_all_reduce
 
-print("X")
+    # input_tensors_numpy = [np.random.uniform(0, 1.0, (batch_size, 32, 32, 20)) for _ in range(len(available_devices))]
+    # # gen_nccl_ops.nccl_all_reduce
+
+    print("X")
