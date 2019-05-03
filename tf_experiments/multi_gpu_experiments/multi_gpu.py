94d0428f853ceab3fb3e8a2b424c590304319d36
ufukcbicici <ufukcbicici@yahoo.com>
2019-05-03 11:01:42 +0300

    CIGJ with Gumbel Softmax

--- tf_experiments/multi_gpu_experiments/multi_gpu.py
+++ tf_experiments/multi_gpu_experiments/multi_gpu.py
@@ -6,6 +6,7 @@ from tensorflow.contrib.nccl.ops import gen_nccl_ops
 from auxillary.general_utility_funcs import UtilityFuncs
 
 available_devices = UtilityFuncs.get_available_devices()
+print(available_devices)
 batch_size = 250
 input_tensors = []
 for device_id, device in enumerate(available_devices):
@@ -13,6 +14,6 @@ for device_id, device in enumerate(available_devices):
     input_tensors.append(input_tensor)
 
 input_tensors_numpy = [np.random.uniform(0, 1.0, (batch_size, 32, 32, 20)) for _ in range(len(available_devices))]
-gen_nccl_ops.nccl_all_reduce
+# gen_nccl_ops.nccl_all_reduce
 
 print("X")
