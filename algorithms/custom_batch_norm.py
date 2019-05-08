fe1c438558060cbe9193a645ff3dc35d509e8e32
ufuk.bicici <ufukcbicici@yahoo.com>
2019-05-08 17:22:43 +0300

    Gumbel Softmax CIGJ

--- algorithms/custom_batch_norm.py
+++ algorithms/custom_batch_norm.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 
 from auxillary.general_utility_funcs import UtilityFuncs
 from simple_tf.global_params import GlobalConstants
+from simple_tf.resnet_experiments.resnet_generator import ResnetGenerator
 
 
 class CustomBatchNorm:
@@ -53,20 +54,48 @@ channels = 64
 _x = tf.placeholder(name="input", dtype=tf.float32, shape=(batch_size, width, height, channels))
 is_train = tf.placeholder(name="is_train", dtype=tf.int32)
 
-mu, sigma, normalized_x = CustomBatchNorm.batch_norm(input_tensor=_x,
-                                                     momentum=GlobalConstants.BATCH_NORM_DECAY,
-                                                     epsilon=1e-3,
-                                                     is_training=is_train)
-tf_normalized_x = tf.layers.batch_normalization(inputs=_x,
-                                                momentum=GlobalConstants.BATCH_NORM_DECAY,
-                                                epsilon=1e-3,
-                                                training=tf.cast(is_train, tf.bool))
+tower_count = 4
+strides = GlobalConstants.RESNET_HYPERPARAMS.strides
+activate_before_residual = GlobalConstants.RESNET_HYPERPARAMS.activate_before_residual
+filters = GlobalConstants.RESNET_HYPERPARAMS.num_of_features_per_block
+num_of_units_per_block = GlobalConstants.RESNET_HYPERPARAMS.num_residual_units
+relu_leakiness = GlobalConstants.RESNET_HYPERPARAMS.relu_leakiness
+first_conv_filter_size = GlobalConstants.RESNET_HYPERPARAMS.first_conv_filter_size
 
-sess = tf.Session()
-init = tf.global_variables_initializer()
-sess.run(init)
-
-x = np.random.uniform(size=(batch_size, width, height, channels))
-
-res = sess.run([mu, sigma, normalized_x, tf_normalized_x], feed_dict={_x: x, is_train: 1})
+for tower_id in range(tower_count):
+    with tf.device("/cpu:0"):
+        with tf.name_scope("tower_{0}".format(tower_id)):
+            net = ResnetGenerator.get_input(input=_x, out_filters=filters[0],
+                                            first_conv_filter_size=first_conv_filter_size)
+            with tf.variable_scope("block_1_0"):
+                x = ResnetGenerator.bottleneck_residual(x=net, in_filter=filters[0], out_filter=filters[1],
+                                                        stride=ResnetGenerator.stride_arr(strides[0]),
+                                                        activate_before_residual=activate_before_residual[0],
+                                                        relu_leakiness=relu_leakiness, is_train=is_train,
+                                                        bn_momentum=GlobalConstants.BATCH_NORM_DECAY)
+            for i in range(num_of_units_per_block - 1):
+                with tf.variable_scope("block_1_{0}".format(i + 1)):
+                    net = ResnetGenerator.bottleneck_residual(x=net, in_filter=filters[1],
+                                                              out_filter=filters[1],
+                                                              stride=ResnetGenerator.stride_arr(1),
+                                                              activate_before_residual=False,
+                                                              relu_leakiness=relu_leakiness, is_train=is_train,
+                                                              bn_momentum=GlobalConstants.BATCH_NORM_DECAY)
 print("X")
+# mu, sigma, normalized_x = CustomBatchNorm.batch_norm(input_tensor=_x,
+#                                                      momentum=GlobalConstants.BATCH_NORM_DECAY,
+#                                                      epsilon=1e-3,
+#                                                      is_training=is_train)
+# tf_normalized_x = tf.layers.batch_normalization(inputs=_x,
+#                                                 momentum=GlobalConstants.BATCH_NORM_DECAY,
+#                                                 epsilon=1e-3,
+#                                                 training=tf.cast(is_train, tf.bool))
+#
+# sess = tf.Session()
+# init = tf.global_variables_initializer()
+# sess.run(init)
+#
+# x = np.random.uniform(size=(batch_size, width, height, channels))
+#
+# res = sess.run([mu, sigma, normalized_x, tf_normalized_x], feed_dict={_x: x, is_train: 1})
+# print("X")
