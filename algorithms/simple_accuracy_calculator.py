5ade3b2867ee0eaa9e2ad59b840ffb4a7712197d
ufukcanbicici <ufukcbicici@yahoo.com>
2019-07-03 20:05:02 +0300

    Multipath Calculation is being implemented.

--- algorithms/simple_accuracy_calculator.py
+++ algorithms/simple_accuracy_calculator.py
@@ -1,3 +1,5 @@
+import numpy as np
+from algorithms.multipath_calculator_v2 import MultipathCalculatorV2
 from auxillary.general_utility_funcs import UtilityFuncs
 from simple_tf.global_params import GlobalConstants
 
@@ -16,17 +18,36 @@ class SimpleAccuracyCalculator:
                                                       use_masking=False,
                                                       leaf_node_collection_names=leaf_node_outputs,
                                                       inner_node_collections_names=inner_node_outputs)
+        leaf_true_labels_dict = leaf_node_collections["label_tensor"]
+        thresholds = GlobalConstants.MULTIPATH_SCHEDULES
+        assert all([np.array])
+        label_list =
+        sample_count = list(leaf_true_labels_dict.values())[0].shape[0]
+        multipath_accuracy_calculator = MultipathCalculatorV2(thread_id=0, run_id=run_id, iteration=iteration,
+                                                              thresholds_list=thresholds, network=network,
+                                                              sample_count=sample_count, )
 
 
 
 
 
 
-        leaf_predicted_labels_dict = {}
-        leaf_true_labels_dict = {}
-        info_gain_dict = {}
-        branch_activations = {}
-        branch_probs = {}
-        one_hot_branch_probs = {}
-        posterior_probs = {}
-        hash_codes = {}
+
+
+
+
+
+
+
+
+
+
+
+        # leaf_predicted_labels_dict = {}
+        # leaf_true_labels_dict = {}
+        # info_gain_dict = {}
+        # branch_activations = {}
+        # branch_probs = {}
+        # one_hot_branch_probs = {}
+        # posterior_probs = {}
+        # hash_codes = {}
