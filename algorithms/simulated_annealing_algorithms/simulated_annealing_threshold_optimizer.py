c1f953fef339351b4f94cd8a337fb2f9ecf0ef6d
ufukcanbicici <ufukcbicici@yahoo.com>
2019-07-09 17:57:16 +0300

    Started implementation of Simulated Annealing

--- algorithms/simulated_annealing_algorithms/simulated_annealing_threshold_optimizer.py
+++ algorithms/simulated_annealing_algorithms/simulated_annealing_threshold_optimizer.py
@@ -7,13 +7,15 @@ class SimulatedAnnealingThresholdOptimizer:
                  annealing_schedule,
                  balance_coefficient,
                  use_weighted_scoring,
-                 multipath_score_calculators):
+                 multipath_score_calculators,
+                 verbose):
         self.network = network
         self.maxNumOfIterations = max_num_of_iterations
         self.annealingSchedule = annealing_schedule
         self.balanceCoefficient = balance_coefficient
         self.useWeightedScoring = use_weighted_scoring
         self.multipathScoreCalculators = multipath_score_calculators
+        self.verbose = verbose
 
     def pick_fully_random_state(self):
         threshold_state = {}
@@ -31,15 +33,21 @@ class SimulatedAnnealingThresholdOptimizer:
 
     def calculate_threshold_score(self, threshold_state):
         scores = []
+        accuracies = []
+        computation_overloads = []
         for scorer in self.multipathScoreCalculators:
             res_method_0, res_method_1 = scorer.calculate_for_threshold(thresholds_dict=threshold_state)
             result = res_method_1 if self.useWeightedScoring else res_method_0
             accuracy_gain = self.balanceCoefficient * result.accuracy
             computation_overload_loss = (1.0 - self.balanceCoefficient) * (result.computationOverload - 1.0)
             score = accuracy_gain - computation_overload_loss
+            accuracies.append(result.accuracy)
+            computation_overloads.append(result.computationOverload)
             scores.append(score)
         final_score = np.mean(np.array(scores))
-        return final_score
+        final_accuracy = np.mean(np.array(accuracies))
+        final_computation_overload = np.mean(np.array(computation_overloads))
+        return final_score, final_accuracy, final_computation_overload
 
     def get_acceptance(self, curr_score, candidate_score):
         if candidate_score > curr_score:
@@ -56,12 +64,26 @@ class SimulatedAnnealingThresholdOptimizer:
     def run(self):
         # Get initial state
         curr_state = self.pick_fully_random_state()
-        curr_score = self.calculate_threshold_score(threshold_state=curr_state)
+        curr_score, curr_accuracy, curr_computation_overload = \
+            self.calculate_threshold_score(threshold_state=curr_state)
         for iteration_id in range(self.maxNumOfIterations):
             # Pick a random neighbor
             candidate_state = self.get_neighbor(threshold_state=curr_state)
-            candidate_score = self.calculate_threshold_score(threshold_state=candidate_state)
+            candidate_score, candidate_accuracy, candidate_computation_overload = \
+                self.calculate_threshold_score(threshold_state=candidate_state)
             accepted = self.get_acceptance(curr_score=curr_score, candidate_score=candidate_score)
+            # Report current state
+            if self.verbose:
+                print("****************Iteration {0}****************".format(iteration_id))
+                print("Current State:{0}".format(curr_state))
+                print("Current Score:{0}".format(curr_score))
+                print("Current Accuracy:{0}".format(curr_accuracy))
+                print("Current Computation Overload:{0}".format(curr_computation_overload))
+                print("Candidate State:{0}".format(candidate_state))
+                print("Candidate Score:{0}".format(candidate_score))
+                print("Candidate Accuracy:{0}".format(candidate_accuracy))
+                print("Candidate Computation Overload:{0}".format(candidate_computation_overload))
+                print("****************Iteration {0}****************".format(iteration_id))
             if accepted:
                 curr_state = candidate_state
                 curr_score = candidate_score
