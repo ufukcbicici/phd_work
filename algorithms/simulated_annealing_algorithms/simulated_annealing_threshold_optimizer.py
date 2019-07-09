250f2ca763898910905eea064f89f8655aea1ff3
ufukcanbicici <ufukcbicici@yahoo.com>
2019-07-09 17:48:42 +0300

    Started implementation of Simulated Annealing

--- algorithms/simulated_annealing_algorithms/simulated_annealing_threshold_optimizer.py
+++ algorithms/simulated_annealing_algorithms/simulated_annealing_threshold_optimizer.py
@@ -38,50 +38,30 @@ class SimulatedAnnealingThresholdOptimizer:
             computation_overload_loss = (1.0 - self.balanceCoefficient) * (result.computationOverload - 1.0)
             score = accuracy_gain - computation_overload_loss
             scores.append(score)
-        final_score = np.mean(np.array_equal(scores))
+        final_score = np.mean(np.array(scores))
         return final_score
+
+    def get_acceptance(self, curr_score, candidate_score):
+        if candidate_score > curr_score:
+            return True
+        else:
+            temperature = self.annealingSchedule.value
+            acceptance_threshold = np.exp((candidate_score - curr_score) / temperature)
+            rand_val = np.random.uniform(low=0.0, high=1.0)
+            if acceptance_threshold >= rand_val:
+                return True
+            else:
+                return False
 
     def run(self):
         # Get initial state
         curr_state = self.pick_fully_random_state()
-        curr_score = self.network
-
+        curr_score = self.calculate_threshold_score(threshold_state=curr_state)
         for iteration_id in range(self.maxNumOfIterations):
             # Pick a random neighbor
             candidate_state = self.get_neighbor(threshold_state=curr_state)
-            rand_val = np.random.uniform(low=0.0, high=1.0)
-            # acceptance_threshold = self.annealingSchedule.value
-            # if acceptance_threshold >= rand_val:
-
-    # @staticmethod
-    # def pick_fully_random_state(network):
-    #     threshold_state = {}
-    #     for node in network.topologicalSortedNodes:
-    #         if node.isLeaf:
-    #             continue
-    #         child_count = len(network.dagObject.children(node=node))
-    #         max_threshold = 1.0 / float(child_count)
-    #         thresholds = max_threshold * np.random.uniform(size=(child_count, ))
-    #         threshold_state[node.index] = thresholds
-    #     return  threshold_state
-    #
-    # @staticmethod
-    # def get_neighbor(network, threshold_state):
-    #     threshold_state = {}
-    #     for node in network.topologicalSortedNodes:
-    #         if node.isLeaf:
-    #             continue
-    #         child_count = len(network.dagObject.children(node=node))
-    #         max_threshold = 1.0 / float(child_count)
-    #         thresholds = max_threshold * np.random.uniform(size=(child_count, ))
-    #         threshold_state[node.index] = thresholds
-    #     return  threshold_state
-    #
-    # @staticmethod
-    # def run_with_uniform_sampling(network, max_num_of_iterations, search_volume_edge_ratio):
-    #     iteration = 0
-    #     # Pick a random threshold state
-    #
-    #
-    #
-    #     while True:
+            candidate_score = self.calculate_threshold_score(threshold_state=candidate_state)
+            accepted = self.get_acceptance(curr_score=curr_score, candidate_score=candidate_score)
+            if accepted:
+                curr_state = candidate_state
+                curr_score = candidate_score
