1affa88e74931aa0c6963d7a99754654c2af76b3
ufukcbicici <ufukcbicici@yahoo.com>
2019-07-10 18:44:49 +0300

    Multipath Evaluation Reimplemented

--- simple_tf/cifar_nets/cifar_entry.py
+++ simple_tf/cifar_nets/cifar_entry.py
@@ -24,8 +24,8 @@ def get_explanation_string(network):
     # Tree
     # explanation = "Resnet-50 CIGN 2 GPUs Parallel Test\n"
     # "(Lr=0.01, - Decay 1/(1 + i*0.0001) at each i. iteration)\n"
-    # explanation = "Resnet-50 CIGN Tests\n"
-    explanation = "Resnet-50 CIGN Random Sampling Routing Tests\n"
+    explanation = "Resnet-50 CIGN Tests\n"
+    # explanation = "Resnet-50 CIGN Random Sampling Routing Tests\n"
     explanation += "Using Fast Tree Version:{0}\n".format(GlobalConstants.USE_FAST_TREE_MODE)
     explanation += "Batch Size:{0}\n".format(GlobalConstants.BATCH_SIZE)
     explanation += "Tree Degree:{0}\n".format(GlobalConstants.TREE_DEGREE_LIST)
@@ -154,8 +154,7 @@ def cifar100_training():
     #                      0.0003, 0.00035, 0.0004, 0.00045, 0.0005] * GlobalConstants.EXPERIMENT_MULTIPLICATION_FACTOR
     # classification_wd = [0.00005, 0.0001] * GlobalConstants.EXPERIMENT_MULTIPLICATION_FACTOR
     # classification_wd = [0.00035, 0.00035, 0.00035, 0.00035, 0.0004, 0.0004, 0.0004, 0.0004]
-    classification_wd = [0.00005, 0.0001, 0.00015, 0.0002, 0.00025, 0.0003, 0.00035, 0.0004, 0.00045, 0.0005] * \
-                        GlobalConstants.EXPERIMENT_MULTIPLICATION_FACTOR
+    classification_wd = [0.00045, 0.0005] * GlobalConstants.EXPERIMENT_MULTIPLICATION_FACTOR
     classification_wd = sorted(classification_wd)
     decision_wd = [0.0]
     info_gain_balance_coeffs = [1.0]
