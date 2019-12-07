223223b542f969fe5f9175da32c7b390224086c5
ufukcanbicici <ufukcbicici@yahoo.com>
2019-12-07 16:42:56 +0300

    Added Multipath Accuracy Calculator for Early Exits.

--- simple_tf/global_params.py
+++ simple_tf/global_params.py
@@ -311,7 +311,7 @@ class GlobalConstants:
     # MULTIPATH_SCHEDULES = [0.5, 0.45, 0.4, 0.35, 0.3, 0.25, 0.2, 0.15, 0.1, 0.05]
     # MULTIPATH_SCHEDULES.extend([i*0.001 for i in range(50)])
     SAVE_PATH_INFO_TO_HD = True
-    EVALUATION_EPOCHS_BEFORE_ENDING = 100
+    EVALUATION_EPOCHS_BEFORE_ENDING = 10
     MULTIPATH_SCHEDULES = [0.5, 0.45, 0.4, 0.35, 0.3, 0.25, 0.2, 0.15,
                            0.1, 0.09, 0.08, 0.07, 0.06, 0.05, 0.04, 0.03, 0.025, 0.02, 0.01,
                            0.009, 0.008, 0.007, 0.006, 0.005, 0.004, 0.003, 0.0025, 0.002, 0.001,
