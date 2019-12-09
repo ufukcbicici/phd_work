4af820ea8226f7438e9934c786f9f9cbcb9c7195
ufukcbicici <ufukcbicici@yahoo.com>
2019-12-09 08:26:07 +0300

    Sampling CIGN Tests

--- simple_tf/fashion_net/fashion_cign_lite_early_exit.py
+++ simple_tf/fashion_net/fashion_cign_lite_early_exit.py
@@ -132,7 +132,7 @@ class FashionCignLiteEarlyExit(EarlyExitTree):
         for v in tf.trainable_variables():
             total_param_count += np.prod(v.get_shape().as_list())
         # Tree
-        explanation = "Fashion Net - Early Exit v2.0\n"
+        explanation = "Fashion Net - Early Exit v3.0\n"
         # "(Lr=0.01, - Decay 1/(1 + i*0.0001) at each i. iteration)\n"
         explanation += "Using Fast Tree Version:{0}\n".format(GlobalConstants.USE_FAST_TREE_MODE)
         explanation += "Batch Size:{0}\n".format(GlobalConstants.BATCH_SIZE)
