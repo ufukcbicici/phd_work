0d946034193e38cde2372a5fcdd6ba06e7567219
ufukcanbicici <ufukcbicici@yahoo.com>
2020-01-27 14:42:30 +0300

    Reward system has been implemented.

--- algorithms/roi_pooling.py
+++ algorithms/roi_pooling.py
@@ -54,15 +54,51 @@ class RoIPooling:
     #     self.pooledWidth = pooled_width
     #     self.pooledHeight = pooled_height
     @staticmethod
-    def roi_pool(x, pooled_width, pooled_height):
-        def curried_pool_rois(_x):
-            s0 = tf.reduce_sum(_x[0])
-            s1 = tf.reduce_sum(_x[1])
-            s0 = tf.expand_dims(s0, axis=0)
-            s1 = tf.expand_dims(s1, axis=0)
-            tf_arr = tf.concat([s0, s1], axis=0)
-            return tf_arr
-        pooled_areas = tf.map_fn(curried_pool_rois, x, dtype=tf.float32)
+    def pool_roi_single_f_map_single_roi(feature_map, roi, pooled_height, pooled_width):
+        # Feature map: Single feature map
+        # roi: Single region of interest
+        feature_map_height = int(feature_map.shape[0])
+        feature_map_width = int(feature_map.shape[1])
+        h_start = tf.cast(feature_map_height * roi[0], 'int32')
+        w_start = tf.cast(feature_map_width * roi[1], 'int32')
+        h_end = tf.cast(feature_map_height * roi[2], 'int32')
+        w_end = tf.cast(feature_map_width * roi[3], 'int32')
+        region = feature_map[h_start:h_end, w_start:w_end, :]
+        # Divide the region into non overlapping areas
+        region_height = h_end - h_start
+        region_width = w_end - w_start
+        h_step = tf.cast(region_height / pooled_height, 'int32')
+        w_step = tf.cast(region_width / pooled_width, 'int32')
+        areas = [[(i * h_step, j * w_step,
+                   (i + 1) * h_step if i + 1 < pooled_height else region_height,
+                   (j + 1) * w_step if j + 1 < pooled_width else region_width)
+                  for j in range(pooled_width)]
+                 for i in range(pooled_height)]
+        pooled_areas = [[tf.math.reduce_max(region[x[0]:x[2], x[1]:x[3], :], axis=[0, 1]) for x in row]
+                        for row in areas]
+        pooled_features = tf.stack(pooled_areas)
+        return pooled_features
+
+    @staticmethod
+    def pool_roi_single_f_map_many_rois(feature_map, rois, pooled_height, pooled_width):
+        """ Applies ROI pooling for a single feature map and more than one ROIs
+        """
+
+        def fn(roi):
+            return RoIPooling.pool_roi_single_f_map_single_roi(feature_map, roi, pooled_height, pooled_width)
+
+        pooled_areas = tf.map_fn(fn, rois, dtype=tf.float32)
+        return pooled_areas
+
+    @staticmethod
+    def roi_pool(x, pooled_height, pooled_width):
+
+        def fn(_x):
+            return RoIPooling.pool_roi_single_f_map_many_rois(feature_map=_x[0], rois=_x[1],
+                                                              pooled_height=pooled_height,
+                                                              pooled_width=pooled_width)
+
+        pooled_areas = tf.map_fn(fn, x, dtype=tf.float32)
         return pooled_areas
 
 
@@ -83,24 +119,67 @@ def get_dummy_roi_tensor():
     return roi_arr
 
 
-def main():
+def test_roi_pooling_layer():
+    pooled_height = 7
+    pooled_width = 7
     input_tensor = tf.placeholder(dtype=tf.float32, shape=[None, IMG_W, IMG_H, FEATURE_COUNT])
     rois_tensor = tf.placeholder(tf.float32, shape=(None, NUM_ROIS, 4))
-    # split_features, split_rois, roi_tensors = RoIPooling.roi_pool(feature_map=input_tensor, roi_list=rois_tensor)
-    # RoIPooling.roi_pool(x=input_tensor, roi_list=ROI_LIST)
-
     # Prepare dummy image data
     random_imgs = np.random.uniform(size=(BATCH_SIZE, IMG_W, IMG_H, FEATURE_COUNT))
     # Prepare dummy roi data
     roi_arr = get_dummy_roi_tensor()
 
     # Network
-    roi_output = RoIPooling.roi_pool(x=[input_tensor, rois_tensor], pooled_height=7, pooled_width=7)
+    roi_output = RoIPooling.roi_pool(x=[input_tensor, rois_tensor],
+                                     pooled_height=pooled_height, pooled_width=pooled_width)
 
+    # Get the Tensorflow result.
     sess = tf.Session()
-    results = sess.run([roi_output],  feed_dict={input_tensor: random_imgs, rois_tensor: roi_arr})
+    results = sess.run([roi_output], feed_dict={input_tensor: random_imgs, rois_tensor: roi_arr})
+    tf_result = results[0]
+
+    # Calculate a manual simulation, with very basic for loops
+    pooled_imgs = []
+    for img_idx in range(random_imgs.shape[0]):
+        feature_map = random_imgs[img_idx]
+        img_rois = roi_arr[img_idx]
+        pooled_maps = []
+        for roi_idx in range(img_rois.shape[0]):
+            roi = img_rois[roi_idx]
+            feature_map_height = int(feature_map.shape[0])
+            feature_map_width = int(feature_map.shape[1])
+            h_start = int(feature_map_height * roi[0])
+            w_start = int(feature_map_width * roi[1])
+            h_end = int(feature_map_height * roi[2])
+            w_end = int(feature_map_width * roi[3])
+            region = feature_map[h_start:h_end, w_start:w_end, :]
+            # Divide the region into non overlapping areas
+            region_height = h_end - h_start
+            region_width = w_end - w_start
+            h_step = int(region_height / pooled_height)
+            w_step = int(region_width / pooled_width)
+            pooled_map = np.zeros(shape=(pooled_height, pooled_width, region.shape[-1]))
+            for i in range(pooled_height):
+                delta_h = h_step if i != pooled_height - 1 else region_height - i * h_step
+                for j in range(pooled_width):
+                    delta_w = w_step if j != pooled_width - 1 else pooled_width - j * w_step
+                    sub_region = region[i*delta_h:(i+1)*delta_h, j*delta_w:(j+1)*delta_w, :]
+                    max_val = np.max(sub_region, axis=[1, 2])
+                    pooled_map[i, j, :] = max_val
+            pooled_maps.append(pooled_map)
+        pooled_maps = np.stack(pooled_maps, axis=0)
+        pooled_imgs.append(pooled_maps)
+    np_result = np.stack(pooled_imgs, axis=0)
+
+    assert np.allclose(tf_result, np_result)
+    print("X")
+
+
+def main():
+    test_roi_pooling_layer()
     print("X")
 
 
 if __name__ == "__main__":
     main()
+
