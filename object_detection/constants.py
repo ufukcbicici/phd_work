4ddf1edcb212b3f67ada243b844db6a456873e70
ufuk <ufukcbicici@yahoo.com>
2020-02-02 04:11:30 +0300

    Implementing a more general Policy Gradients algorithm for sample routing.

---
+++ object_detection/constants.py
@@ -0,0 +1,5 @@
+class Constants:
+    def __init__(self):
+        pass
+
+    IMG_WIDTHS = [300, 400, 500, 600, 700, 800]
--- object_detection/object_detection_data_manager.py
+++ object_detection/object_detection_data_manager.py
@@ -25,6 +25,7 @@ class ObjectDetectionDataManager:
         assert len(img_files) == len(txt_files)
         self.dataList = []
         data_dict = {}
+        # Read images
         for f in img_files:
             filename, file_extension = os.path.splitext(f)
             file_path = os.path.join(self.dataPath, f)
@@ -33,5 +34,31 @@ class ObjectDetectionDataManager:
             img_obj = DetectionImage(img_name=filename, img_arr=img_arr)
             assert filename not in data_dict
             data_dict[filename] = img_obj
+        img_widths = np.array([img_obj.imgArr.shape[1] for img_obj in data_dict.values()])
+        img_heights = np.array([img_obj.imgArr.shape[0] for img_obj in data_dict.values()])
+        # Read ROIs
+        for f in txt_files:
+            filename, file_extension = os.path.splitext(f)
+            file_path = os.path.join(self.dataPath, f)
+            roi_file = open(file_path, 'r')
+            list_of_rois = roi_file.readlines()
+            list_of_rois = [line.replace("\n", "") for line in list_of_rois]
+            roi_arr = []
+            for roi_str in list_of_rois:
+                roi_parts = roi_str.split(" ")
+                assert len(roi_parts) == 5
+                roi_tpl = np.array([float(x) for x in roi_parts])
+                roi_arr.append(roi_tpl)
+            roi_arr = np.stack(roi_arr, axis=0)
+            assert filename in data_dict
+            data_dict[filename].objList = roi_arr
+        print("X")
+
+            # img_arr = cv2.imread(filename=file_path)
+            # assert img_arr is not None and len(img_arr.shape) == 3 and img_arr.shape[0] > 0 and img_arr.shape[1] > 0
+            # img_obj = DetectionImage(img_name=filename, img_arr=img_arr)
+            # assert filename not in data_dict
+            # data_dict[filename] = img_obj
+
         print("X")
 
