f9e1250ab89d4d847628840fc4aca713eb35da7c
ufukcanbicici <ufukcbicici@yahoo.com>
2020-02-03 18:00:13 +0300

    Reward system has been implemented.

--- object_detection/bb_clustering.py
+++ object_detection/bb_clustering.py
@@ -16,13 +16,16 @@ class BBClustering:
         roi_comparison_list = []
         max_covering_bb_list = []
         for idx, bb in enumerate(roi_list):
-            iou_list = np.array(
-                sorted([BBClustering.get_iou_of_bbs(bb, roi_list[j]) for j in range(roi_list.shape[0])],
-                       reverse=True))
+            # iou_list = np.array(
+            #     sorted([BBClustering.get_iou_of_bbs(bb, roi_list[j]) for j in range(roi_list.shape[0])],
+            #            reverse=True))
+            iou_list = np.array(sorted(BBClustering.get_iou_of_bbs_vec(bb_x=bb, bb_y_list=roi_list), reverse=True))
+            # assert np.allclose(iou_list, iou_list_vec)
             roi_comparison_list.append(iou_list[iou_list >= iou_threshold])
         while coverage < max_coverage:
-            max_covering_bb_idx = np.argmax(np.array([len(iou_list) for iou_list in roi_comparison_list]))
+            max_covering_bb_idx = int(np.argmax(np.array([len(iou_list) for iou_list in roi_comparison_list])))
             max_covering_bb_list.append(roi_list[max_covering_bb_idx])
+            roi_comparison_list.pop(max_covering_bb_idx)
             print("X")
 
         print("X")
@@ -40,3 +43,19 @@ class BBClustering:
         union_area = bb_1_area + bb_2_area - intersection_area
         iou = intersection_area / union_area
         return iou
+
+    @staticmethod
+    def get_iou_of_bbs_vec(bb_x, bb_y_list):
+        bb_x_coords = np.expand_dims(np.array([-0.5 * bb_x[0], -0.5 * bb_x[1], 0.5 * bb_x[0], 0.5 * bb_x[1]]), axis=0)
+        bb_x_coords = np.repeat(bb_x_coords, axis=0, repeats=bb_y_list.shape[0])
+        bb_y_coord_matrix = np.stack(
+            [-0.5 * bb_y_list[:, 0], -0.5 * bb_y_list[:, 1], 0.5 * bb_y_list[:, 0], 0.5 * bb_y_list[:, 1]], axis=1)
+        intersection_left = np.max(np.stack([bb_x_coords[:, 0], bb_y_coord_matrix[:, 0]], axis=1), axis=1)
+        intersection_top = np.max(np.stack([bb_x_coords[:, 1], bb_y_coord_matrix[:, 1]], axis=1), axis=1)
+        intersection_areas = (-2.0 * intersection_left) * (-2.0 * intersection_top)
+        bb_x_area = np.array((bb_x_coords[0, 2] - bb_x_coords[0, 0]) * (bb_x_coords[0, 3] - bb_x_coords[0, 1]))
+        bb_y_areas = (bb_y_coord_matrix[:, 2] - bb_y_coord_matrix[:, 0]) * \
+                     (bb_y_coord_matrix[:, 3] - bb_y_coord_matrix[:, 1])
+        union_areas = np.expand_dims(bb_x_area, axis=0) + bb_y_areas - intersection_areas
+        iou_list = intersection_areas * np.reciprocal(union_areas)
+        return iou_list
