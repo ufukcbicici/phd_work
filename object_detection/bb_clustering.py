69bc074dae756ddd3cd90877f24774c69722626d
ufukcanbicici <ufukcbicici@yahoo.com>
2020-02-05 14:26:02 +0300

    Reward system has been implemented.

--- object_detection/bb_clustering.py
+++ object_detection/bb_clustering.py
@@ -32,49 +32,30 @@ class BBClustering:
         return total_cost
 
     def k_medoids(self, medoid_count, iou_distance_matrix, max_num_of_iterations=100):
-        curr_medoids = np.sort(np.random.choice(iou_distance_matrix.shape[0], medoid_count, replace=False))
+        curr_medoids = np.random.choice(iou_distance_matrix.shape[0], medoid_count, replace=False)
         best_cost = BBClustering.get_configuration_cost(iou_distance_matrix[:, curr_medoids])
         for iteration_id in range(max_num_of_iterations):
+            medoids_changed = False
             medoid_member_pairs = UtilityFuncs.get_cartesian_product(
                 [np.arange(medoid_count), np.arange(iou_distance_matrix.shape[0])])
+            print("*******Iteration:{0} Medoids:{1} Cost:{2}*******".format(iteration_id, curr_medoids, best_cost))
             for mo_pair in medoid_member_pairs:
                 medoid_idx = mo_pair[0]
                 object_idx = mo_pair[1]
+                if np.any(curr_medoids == object_idx):
+                    continue
                 new_medoids = np.copy(curr_medoids)
                 new_medoids[medoid_idx] = object_idx
-
-
-
-
-
-        # for iteration_id in range(max_num_of_iterations):
-        #     # Medoid assignment step
-        #     distances_to_medoids = iou_distance_matrix[medoid_indices]
-        #     medoid_assignments = np.argmin(distances_to_medoids, axis=1)
-
-
-
-
-
-
-
-        # coverage = 0.0
-        # roi_comparison_list = []
-        # max_covering_bb_list = []
-        # for idx, bb in enumerate(roi_list):
-        #     # iou_list = np.array(
-        #     #     sorted([BBClustering.get_iou_of_bbs(bb, roi_list[j]) for j in range(roi_list.shape[0])],
-        #     #            reverse=True))
-        #     iou_list = np.array(sorted(BBClustering.get_iou_of_bbs_vec(bb_x=bb, bb_y_list=roi_list), reverse=True))
-        #     # assert np.allclose(iou_list, iou_list_vec)
-        #     roi_comparison_list.append(iou_list[iou_list >= iou_threshold])
-        # while coverage < max_coverage:
-        #     max_covering_bb_idx = int(np.argmax(np.array([len(iou_list) for iou_list in roi_comparison_list])))
-        #     max_covering_bb_list.append(roi_list[max_covering_bb_idx])
-        #     roi_comparison_list.pop(max_covering_bb_idx)
-        #     print("X")
-
-        print("X")
+                cost = BBClustering.get_configuration_cost(iou_distance_matrix[:, new_medoids])
+                if cost < best_cost:
+                    best_cost = cost
+                    curr_medoids = new_medoids
+                    print("*******Iteration:{0} Medoids:{1} Cost:{2}*******".format(iteration_id, curr_medoids,
+                                                                                    best_cost))
+                    medoids_changed = True
+            if not medoids_changed:
+                break
+        return curr_medoids
 
     @staticmethod
     def get_iou_of_bbs(bb_1, bb_2):
