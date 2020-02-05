7f69c8f52e2ca681a4d16c974f882c134c2ddd47
ufukcanbicici <ufukcbicici@yahoo.com>
2020-02-05 18:58:14 +0300

    Reward system has been implemented.

--- object_detection/main.py
+++ object_detection/main.py
@@ -6,18 +6,39 @@ from object_detection.constants import Constants
 from object_detection.object_detection_data_manager import ObjectDetectionDataManager
 
 
-def main():
+# Fast R-CNN Module Entry Points
+def create_dataset(iou_threshold, max_coverage, test_ratio):
     curr_path = os.path.dirname(os.path.abspath(__file__))
     path = os.path.join(os.path.join(os.path.join(curr_path, ".."), "data"), "tuborgData")
+    dataset = ObjectDetectionDataManager(path=path)
+    dataset.read_data()
+    dataset.process_data(iou_threshold=iou_threshold, max_coverage=max_coverage, test_ratio=test_ratio)
+    dataset.save_processed_data()
+
+
+def load_dataset():
+    curr_path = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(os.path.join(os.path.join(curr_path, ".."), "data"), "tuborgData")
+    dataset = ObjectDetectionDataManager.load_processed_data(data_path=path)
+
+
+def main():
+    create_dataset(iou_threshold=Constants.POSITIVE_IOU_THRESHOLD, max_coverage=Constants.MAX_INCLUSIVENESS_BB,
+                   test_ratio=0.15)
+
+    # curr_path = os.path.dirname(os.path.abspath(__file__))
+    # path = os.path.join(os.path.join(os.path.join(curr_path, ".."), "data"), "tuborgData")
+
     # dataset = ObjectDetectionDataManager(path=path)
     # dataset.read_data()
     # dataset.process_data()
     # dataset.save_processed_data()
-    dataset = ObjectDetectionDataManager.load_processed_data(data_path=path)
-    # dataset.show_image(img_obj=dataset.dataList[10], scale=640)
-    bb_clustering_algorithm = BBClustering(dataset=dataset)
-    bb_clustering_algorithm.run(iou_threshold=Constants.POSITIVE_IOU_THRESHOLD,
-                                max_coverage=Constants.MAX_INCLUSIVENESS_BB)
+
+    # dataset = ObjectDetectionDataManager.load_processed_data(data_path=path)
+    # # dataset.show_image(img_obj=dataset.dataList[10], scale=640)
+    # bb_clustering_algorithm = BBClustering(dataset=dataset)
+    # bb_clustering_algorithm.run(iou_threshold=Constants.POSITIVE_IOU_THRESHOLD,
+    #                             max_coverage=Constants.MAX_INCLUSIVENESS_BB)
 
 
 if __name__ == "__main__":
