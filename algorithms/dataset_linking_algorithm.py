7e3958e436c0ba789239506b64cc10b385586bdb
ufuk <ufukcbicici@yahoo.com>
2020-03-15 07:32:09 +0300

    Q Learning

--- algorithms/dataset_linking_algorithm.py
+++ algorithms/dataset_linking_algorithm.py
@@ -2,7 +2,7 @@ import numpy as np
 import os
 import pickle
 from scipy.spatial import distance_matrix
-
+from auxillary.db_logger import DbLogger
 from simple_tf.cign.fast_tree import FastTreeNetwork
 
 network_id = 453
@@ -19,7 +19,7 @@ class DatasetLinkingAlgorithm:
         pass
 
     @staticmethod
-    def calculate_distance_matrices():
+    def run():
         data_dict = {}
         # Load all the data
         for iteration in iterations:
@@ -27,39 +27,53 @@ class DatasetLinkingAlgorithm:
             routing_data = network.load_routing_info(run_id=network_id, iteration=iteration, data_type="test",
                                                      output_names=output_names)
             data_dict[iteration] = routing_data
-        # Link all iterations
-        for idx, iteration in enumerate(iterations):
-            if iteration == iterations[-1]:
-                break
-            curr_iteration_data = data_dict[iterations[idx]]
-            next_iteration_data = data_dict[iterations[idx + 1]]
-            for feature_name in used_output_names:
-                curr_iteration_feature_dict = curr_iteration_data.get_dict(feature_name)
-                next_iteration_feature_dict = next_iteration_data.get_dict(feature_name)
-                assert set(curr_iteration_feature_dict.keys()) == set(next_iteration_feature_dict.keys())
-                keys = sorted(list(curr_iteration_feature_dict.keys()))
-                for key in keys:
-                    distance_matrix_name = "distance_matrix_{0}_node{1}_i{2}_vs_i{3}.sav".format(
-                        feature_name, key, iterations[idx], iterations[idx + 1])
-                    print("Creating {0}.".format(distance_matrix_name))
-                    X_curr = curr_iteration_feature_dict[key]
-                    X_next = next_iteration_feature_dict[key]
-                    assert X_curr.shape == X_next.shape
-                    X_curr = np.reshape(X_curr, newshape=(X_curr.shape[0], np.prod(X_curr.shape[1:])))
-                    X_next = np.reshape(X_next, newshape=(X_next.shape[0], np.prod(X_next.shape[1:])))
-                    d_M = distance_matrix(X_curr, X_next)
-                    directory_path = FastTreeNetwork.get_routing_info_path(network_name=network_name,
-                                                                           run_id=network_id, iteration=iteration,
-                                                                           data_type="test")
-                    pickle.dump(d_M, open(os.path.abspath(os.path.join(directory_path, distance_matrix_name)), "wb"))
-                    print("{0} has been created.".format(distance_matrix_name))
-            print("X")
+        data_length_set = set([data.labelList.shape[0] for data in data_dict.values()])
+        assert len(data_length_set) == 1
+        sample_count = list(data_length_set)[0]
+
+        # Link all samples among all iterations
+        for sample_id in range(sample_count):
+            sample_id_in_iterations = {iterations[0]: sample_id}
+            for idx, iteration in enumerate(iterations):
+                sample_id_in_curr_iteration = sample_id_in_iterations[iteration]
+                curr_iteration_data = data_dict[iterations[idx]]
+                next_iteration_data = data_dict[iterations[idx + 1]]
+                for feature_name in used_output_names:
+                    curr_iteration_feature_dict = curr_iteration_data.get_dict(feature_name)
+                    next_iteration_feature_dict = next_iteration_data.get_dict(feature_name)
+                    assert set(curr_iteration_feature_dict.keys()) == set(next_iteration_feature_dict.keys())
+                    node_ids = sorted(list(curr_iteration_feature_dict.keys()))
+                    for node_id in node_ids:
+                        X_curr = curr_iteration_feature_dict[node_id]
+                        X_next = next_iteration_feature_dict[node_id]
+                        X_curr = np.reshape(X_curr, newshape=(X_curr.shape[0], np.prod(X_curr.shape[1:])))
+                        X_next = np.reshape(X_next, newshape=(X_next.shape[0], np.prod(X_next.shape[1:])))
+                        x_curr = X_curr[sample_id_in_curr_iteration]
+                        dif_matrix = x_curr - X_next
+                        squared_dif_matrix = np.square(dif_matrix)
+                        squared_distances = np.sum(squared_dif_matrix, axis=1)
+                        arg_sort_indices = np.argsort(squared_distances)
+                        assert squared_distances[arg_sort_indices[0]] < squared_distances[arg_sort_indices[1]]
+                        print("SampleId:{0} CurrIteration:{1} NextIteration:{2} FeatureName:{3} NodeId:{4}".format(
+                            sample_id, iterations[idx], iterations[idx + 1], feature_name, node_id))
+                        print("arg_sort_indices[0:10]:{0}".format(arg_sort_indices[0:10]))
+                        print("squared_distances[arg_sort_indices[0:10]]:{0}".format(
+                            squared_distances[arg_sort_indices[0:10]]))
+                        sample_id_in_next_iteration = arg_sort_indices[0]
+                        if iterations[idx + 1] not in sample_id_in_iterations:
+                            sample_id_in_iterations[iterations[idx + 1]] = sample_id_in_next_iteration
+                        else:
+                            assert sample_id_in_iterations[iterations[idx + 1]] == sample_id_in_next_iteration
+                        x_next = X_next[sample_id_in_next_iteration]
+                        assert np.allclose(np.sum(np.square(x_next - x_curr)),
+                                           squared_distances[sample_id_in_next_iteration])
+                        print("X")
 
 
 def main():
     # compare_gpu_implementation()
     # train_basic_q_learning()
-    DatasetLinkingAlgorithm.calculate_distance_matrices()
+    DatasetLinkingAlgorithm.run()
 
 
 if __name__ == "__main__":
