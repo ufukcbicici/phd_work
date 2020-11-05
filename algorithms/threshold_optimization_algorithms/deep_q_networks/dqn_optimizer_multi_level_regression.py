23ad819d2f38fbb5a59c355e222cdc995f17cc20
ufukcbicici <ufukcbicici@yahoo.com>
2020-11-05 04:09:51 +0300

    Adding non deep learning q learning

--- algorithms/threshold_optimization_algorithms/deep_q_networks/dqn_optimizer_multi_level_regression.py
+++ algorithms/threshold_optimization_algorithms/deep_q_networks/dqn_optimizer_multi_level_regression.py
@@ -245,6 +245,15 @@ class DqnMultiLevelRegression(DqnWithRegression):
                                                                              action_id=action_id,
                                                                              R_table=Q_pred)
                 q_estimated[:, action_id, :] = Q_pred_converted
+            estimated_q_tables.append(q_estimated)
+        _, _, training_accuracy, training_computation_cost = self.execute_bellman_equation(
+            Q_tables=estimated_q_tables, sample_indices=self.routingDataset.trainingIndices)
+        _, _, test_accuracy, test_computation_cost = self.execute_bellman_equation(
+            Q_tables=estimated_q_tables, sample_indices=self.routingDataset.testIndices)
+        print("training_accuracy:{0} training_computation_cost:{1}".format(
+            training_accuracy, training_computation_cost))
+        print("test_accuracy:{0} test_computation_cost:{1}".format(
+            test_accuracy, test_computation_cost))
 
     def train_non_deep_learning_classification(self, **kwargs):
         tf.reset_default_graph()
