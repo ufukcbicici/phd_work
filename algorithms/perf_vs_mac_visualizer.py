a5329331359ced4d3f5885d3ba20277f1aef4539
Ufuk <ufuk.bicici@bragi.com>
2021-04-11 15:07:05 +0300

    -Just before debugging.

--- algorithms/perf_vs_mac_visualizer.py
+++ algorithms/perf_vs_mac_visualizer.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from matplotlib import rc, rcParams
 
 
 class PerfvsMacGraph:
@@ -9,30 +10,46 @@ class PerfvsMacGraph:
     def draw(lambda_list, accuracy_list, mac_cost):
         fig, ax1 = plt.subplots()
 
+        # activate latex text rendering
+        # rc('text', usetex=True)
+        # rc('font', weight='bold')
+        # rcParams['text.latex.preamble'] = [r'\usepackage{sfmath} \boldmath']
+
         color = 'tab:red'
-        ax1.set_xlabel('lambda (λ)')
-        ax1.set_ylabel('Accuracy (%)', color=color)
-        ax1.plot(lambda_list, accuracy_list, color=color)
-        ax1.tick_params(axis='y', labelcolor=color)
+        ax1.set_xlabel('lambda (λ)', fontsize="xx-large", fontweight='bold')
+        ax1.tick_params(axis='x', labelsize="xx-large")
+
+        ax1.set_ylabel('Accuracy (%)', color=color, fontsize="xx-large", fontweight='bold')
+        lns1 = ax1.plot(lambda_list, accuracy_list, color=color, label='Accuracy (%)', linewidth=5)
+        ax1.tick_params(axis='y', labelcolor=color, labelsize=18)
 
         ax2 = ax1.twinx()
 
         color = 'tab:blue'
-        ax2.set_ylabel('Mac Cost (%)', color=color)  # we already handled the x-label with ax1
-        ax2.plot(lambda_list, mac_cost, color=color)
-        ax2.tick_params(axis='y', labelcolor=color)
+        ax2.set_ylabel('Mac Cost (%)', fontsize="xx-large", color=color, fontweight='bold')  # we already handled the x-label with ax1
+        lns2 = ax2.plot(lambda_list, mac_cost, color=color, label='Mac Cost (%)', linewidth=5)
+        ax2.tick_params(axis='y', labelcolor=color, labelsize=18)
+
+        lns = lns1 + lns2
+        labs = [l.get_label() for l in lns]
+        ax1.legend(lns, labs, loc=0, fontsize="xx-large")
+        # ax1.legend(loc=0)
+        # ax2.legend(loc=1)
 
         fig.tight_layout()  # otherwise the right y-label is slightly clipped
+        # plt.xticks(weight="bold")
         plt.show()
 
 
 lambda_list = [1.00, 0.99, 0.95, 0.9, 0.85, 0.8, 0.75, 0.7, 0.65, 0.6]
-accuracy_list = [92.7167, 92.6304, 92.5526, 92.5194, 92.5000, 92.4896, 92.4802, 92.4760, 92.4692, 92.4578]
-mac_cost = [40.9209, 7.3044, 1.4018, 0.6435, 0.3748, 0.2569, 0.1891, 0.1452, 0.1315, 0.0973]
+# accuracy_list = [92.7167, 92.6304, 92.5526, 92.5194, 92.5000, 92.4896, 92.4802, 92.4760, 92.4692, 92.4578]
+# mac_cost = [40.9209, 7.3044, 1.4018, 0.6435, 0.3748, 0.2569, 0.1891, 0.1452, 0.1315, 0.0973]
 
 # accuracy_list = [99.4596, 99.4338, 99.4102, 99.4009, 99.4005, 99.4006, 99.4021, 99.4034, 99.4026, 99.4015]
 # mac_cost = [31.9819, 3.0561, 0.3929, 0.2489, 0.2136, 0.1288, 0.0917, 0.0640, 0.0416, 0.0340]
 
+accuracy_list = [73.63, 73.61, 73.57, 73.51, 73.38, 73.25, 73.17, 73.10, 73.05, 73.01]
+mac_cost = [3.79, 3.66, 3.05, 2.43, 1.5, 1.001, 0.591, 0.412, 0.189, 0.09]
+
+
 PerfvsMacGraph.draw(lambda_list=lambda_list, accuracy_list=accuracy_list, mac_cost=mac_cost)
-
-
--- tf_2_cign/experiments/tf_scatter_nd_gather_nd_experiments.py
+++ tf_2_cign/experiments/tf_scatter_nd_gather_nd_experiments.py
@@ -5,74 +5,95 @@ import time
 batch_size_param = 128
 node_degree = 2
 x = np.random.uniform(size=(batch_size_param, 32))
+conv_x = np.random.uniform(size=(batch_size_param, 16, 16, 32))
 routing_values = np.random.uniform(size=(batch_size_param, node_degree), low=-10.0, high=10.0)
 
 gpus = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(gpus[0], True)
 
-with tf.device('/GPU:0'):
-    # Init
-    tf_x = tf.keras.Input(shape=(32, ), name="tf_x")
-    tf_router = tf.keras.Input(shape=(node_degree, ), name="tf_router")
-    tf_temperature = tf.keras.Input(shape=(), name="tf_temperature")
+# with tf.device('/GPU:0'):
+conv_input = tf.keras.Input(shape=(16, 16, 32), name="conv_input")
+conv_net = conv_input
+conv_net = tf.keras.layers.Conv2D(64, 3, padding="same")(conv_net)
+conv_net = tf.keras.layers.BatchNormalization()(conv_net)
+conv_net = tf.keras.layers.LeakyReLU(alpha=0.2)(conv_net)
+conv_output = tf.keras.layers.MaxPool2D(pool_size=2, strides=2)(conv_net)
 
-    tempered_values = tf_router / tf_temperature
-    p_n_given_x = tf.nn.softmax(tempered_values)
-    tf_batch_size = tf.shape(tf_x)[0]
-    batch_indices = tf.range(0, tf_batch_size, 1)
-    routing_matrix = tf.one_hot(tf.argmax(p_n_given_x, axis=1), node_degree)
+# conv_x = np.random.uniform(size=(batch_size_param, 16, 16, 32))
+# model_conv = tf.keras.Model(inputs={"conv_input": conv_input},  outputs=[conv_output])
+# results = model_conv(inputs={"conv_input": conv_x}, training=False)
+# print("X")
+
+# Init
+tf_x = tf.keras.Input(shape=(32, ), name="tf_x")
+tf_router = tf.keras.Input(shape=(node_degree, ), name="tf_router")
+tf_temperature = tf.keras.Input(shape=(), name="tf_temperature")
+
+tempered_values = tf_router / tf_temperature
+p_n_given_x = tf.nn.softmax(tempered_values)
+tf_batch_size = tf.shape(tf_x)[0]
+batch_indices = tf.range(0, tf_batch_size, 1)
+routing_matrix = tf.one_hot(tf.argmax(p_n_given_x, axis=1), node_degree)
 
 
-    mask_vectors = [routing_matrix[:, c] for c in range(node_degree)]
-    batch_indices_masked = [tf.boolean_mask(batch_indices, mask_vectors[c]) for c in range(node_degree)]
-    x_list = [tf.boolean_mask(tf_x, mask_vectors[c]) for c in range(node_degree)]
-    # scatter_nd_shapes = [[tf_batch_size] + x_list[c].shape[1:] for c in range(node_degree)]
-    scatter_nd_shapes = [tf.concat([tf.expand_dims(tf_batch_size, axis=0), tf.shape(x_list[c])[1:]], axis=0)
+mask_vectors = [routing_matrix[:, c] for c in range(node_degree)]
+batch_indices_masked = [tf.boolean_mask(batch_indices, mask_vectors[c]) for c in range(node_degree)]
+x_list = [tf.boolean_mask(tf_x, mask_vectors[c]) for c in range(node_degree)]
+# scatter_nd_shapes = [[tf_batch_size] + x_list[c].shape[1:] for c in range(node_degree)]
+scatter_nd_shapes = [tf.concat([tf.expand_dims(tf_batch_size, axis=0), tf.shape(x_list[c])[1:]], axis=0)
+                     for c in range(node_degree)]
+x_list_scattered = [tf.scatter_nd(tf.expand_dims(batch_indices_masked[c], axis=-1),
+                                  x_list[c], scatter_nd_shapes[c]) for c in range(node_degree)]
+x_concat = tf.concat(x_list_scattered, axis=-1)
+x_list_split = [x_concat[..., c*x_list[c].shape[-1]:(c+1)*x_list[c].shape[-1]] for c in range(node_degree)]
+x_list_split_gathered = [tf.gather_nd(x_list_split[c], tf.expand_dims(batch_indices_masked[c], axis=-1))
                          for c in range(node_degree)]
-    x_list_scattered = [tf.scatter_nd(tf.expand_dims(batch_indices_masked[c], axis=-1),
-                                      x_list[c], scatter_nd_shapes[c]) for c in range(node_degree)]
-    x_concat = tf.concat(x_list_scattered, axis=-1)
-    x_list_split = [x_concat[..., c*x_list[c].shape[-1]:(c+1)*x_list[c].shape[-1]] for c in range(node_degree)]
-    x_list_split_gathered = [tf.gather_nd(x_list_split[c], tf.expand_dims(batch_indices_masked[c], axis=-1))
-                             for c in range(node_degree)]
 
-    model = tf.keras.Model(inputs={"tf_x": tf_x, "tf_router": tf_router, "tf_temperature": tf_temperature},
-                           outputs=[routing_matrix,
-                                    p_n_given_x,
-                                    mask_vectors,
-                                    batch_indices_masked,
-                                    x_list,
-                                    x_list_scattered,
-                                    x_concat,
-                                    x_list_split,
-                                    x_list_split_gathered,
-                                    tf_temperature])
+model = tf.keras.Model(inputs={"tf_x": tf_x, "tf_router": tf_router,
+                               "tf_temperature": tf_temperature, "conv_input": conv_input},
+                       outputs=[routing_matrix,
+                                p_n_given_x,
+                                mask_vectors,
+                                batch_indices_masked,
+                                x_list,
+                                x_list_scattered,
+                                x_concat,
+                                x_list_split,
+                                x_list_split_gathered,
+                                tf_temperature,
+                                conv_output])
 
-    trial_count = 1000
-    for trial_id in range(trial_count):
-        t0 = time.time()
-        print("Trial:{0}".format(trial_id))
-        x = np.random.uniform(size=(batch_size_param, 32))
-        routing_values = np.random.uniform(size=(batch_size_param, node_degree), low=-10.0, high=10.0)
-        results = model(inputs={"tf_x": x, "tf_temperature": 5.0, "tf_router": routing_values}, training=False)
+trial_count = 1000
+for trial_id in range(trial_count):
+    t0 = time.time()
+    print("Trial:{0}".format(trial_id))
+    x = np.random.uniform(size=(batch_size_param, 32))
+    conv_x = np.random.uniform(size=(batch_size_param, 16, 16, 32))
+    routing_values = np.random.uniform(size=(batch_size_param, node_degree), low=-10.0, high=10.0)
+    results = model(inputs={"tf_x": x,
+                            "tf_temperature": 5.0,
+                            "tf_router": routing_values,
+                            "conv_input": conv_x},
+                    training=False)
 
-        # Check if tf.scatter_nd works
-        x_shape = [batch_size_param] + list(x.shape[1:])
-        x_shape[-1] = 2 * x_shape[-1]
-        manuel_x_concat = np.zeros(shape=x_shape)
-        tf_x_concat = results[6].numpy()
-        for c in range(node_degree):
-            indices = results[3][c].numpy()
-            for sample_index in indices:
-                manuel_x_concat[sample_index, ..., c*x.shape[-1]:(c+1)*x.shape[-1]] = x[sample_index]
-        assert np.allclose(tf_x_concat, manuel_x_concat)
+    # Check if tf.scatter_nd works
+    x_shape = [batch_size_param] + list(x.shape[1:])
+    x_shape[-1] = 2 * x_shape[-1]
+    manuel_x_concat = np.zeros(shape=x_shape)
+    tf_x_concat = results[6].numpy()
+    for c in range(node_degree):
+        indices = results[3][c].numpy()
+        for sample_index in indices:
+            manuel_x_concat[sample_index, ..., c*x.shape[-1]:(c+1)*x.shape[-1]] = x[sample_index]
+    assert np.allclose(tf_x_concat, manuel_x_concat)
 
-        # Check if tf.gather_nd works
-        tf_x_before_scatter = results[4]
-        tf_x_split_back = results[8]
-        for c in range(node_degree):
-            assert np.array_equal(tf_x_before_scatter[c].numpy(), tf_x_split_back[c].numpy())
-        t1 = time.time()
-        print("Time Spent:{0}".format(t1 - t0))
+    # Check if tf.gather_nd works
+    tf_x_before_scatter = results[4]
+    tf_x_split_back = results[8]
+    for c in range(node_degree):
+        assert np.array_equal(tf_x_before_scatter[c].numpy(), tf_x_split_back[c].numpy())
+    t1 = time.time()
+    print("Time Spent:{0}".format(t1 - t0))
 
 
 
