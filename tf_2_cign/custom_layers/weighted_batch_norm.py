0da42974fcc195d68b5c76477000bfb091379d5e
Ufuk <ufuk.bicici@bragi.com>
2021-06-04 15:57:01 +0300

    Weighted Batch Norm is implemented and tested.

--- tf_2_cign/custom_layers/weighted_batch_norm.py
+++ tf_2_cign/custom_layers/weighted_batch_norm.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 import time
 
 from tf_2_cign.custom_layers.masked_batch_norm import MaskedBatchNormalization
+tf.autograph.set_verbosity(10, True)
 
 
 class WeightedBatchNormalization(tf.keras.layers.Layer):
@@ -41,6 +42,7 @@ class WeightedBatchNormalization(tf.keras.layers.Layer):
                                            dtype=tf.int32,
                                            trainable=False)
 
+    @tf.function
     def call(self, inputs, **kwargs):
         x_ = inputs[0]
         weight_vector = inputs[1]
@@ -91,70 +93,72 @@ class WeightedBatchNormalization(tf.keras.layers.Layer):
         return normed_x
 
 
-# if __name__ == "__main__":
-#     print(tf.__version__)
-#     gpus = tf.config.list_physical_devices('GPU')
-#     tf.config.experimental.set_memory_growth(gpus[0], True)
-#
-#     with tf.device("GPU"):
-#         batch_size = 125
-#         dim = 128
-#         momentum = 0.9
-#         target_val = 5.0
-#         x1 = np.random.uniform(low=-1.0, high=1.0, size=(batch_size, 32, 32, dim))
-#         x2 = np.random.uniform(low=-1.0, high=1.0, size=(batch_size, 32, 32, dim))
-#         mask_vector = np.random.randint(low=0, high=2, size=(batch_size,))
-#
-#         mb_norm = MaskedBatchNormalization(momentum=momentum)
-#         wb_norm = WeightedBatchNormalization(momentum=momentum)
-#
-#         i_x = tf.keras.Input(shape=(32, 32, dim))
-#         mask_vector_tf = tf.keras.Input(shape=())
-#         # net = tf.keras.layers.Dense(256)(i_x)
-#         net = tf.keras.layers.Conv2D(filters=64,
-#                                      kernel_size=3,
-#                                      activation=tf.nn.relu,
-#                                      strides=1,
-#                                      padding="same",
-#                                      use_bias=True,
-#                                      name="conv")(i_x)
-#         masked_net = tf.boolean_mask(net, mask_vector_tf)
-#         norm_result_mb = mb_norm([net, masked_net])
-#         norm_result_wb = wb_norm([net, mask_vector_tf])
-#
-#         mb_dense = tf.keras.layers.Flatten()(norm_result_mb)
-#         wb_dense = tf.keras.layers.Flatten()(norm_result_wb)
-#
-#         y_hat_mb = tf.reduce_mean(mb_dense, axis=-1)
-#         y_hat_wb = tf.reduce_mean(wb_dense, axis=-1)
-#
-#         y_gt = target_val * tf.ones_like(y_hat_mb)
-#         reconstruction_loss_mb = tf.keras.losses.mean_squared_error(y_gt, y_hat_mb)
-#         reconstruction_loss_wb = tf.keras.losses.mean_squared_error(y_gt, y_hat_wb)
-#         total_loss = 0.5 * (reconstruction_loss_mb + reconstruction_loss_wb)
-#         model = tf.keras.Model(inputs=[i_x, mask_vector_tf],
-#                                outputs={"masked_net": masked_net,
-#                                         "net": net,
-#                                         "norm_result_mb": norm_result_mb,
-#                                         "norm_result_wb": norm_result_wb,
-#                                         "y_hat_mb": y_hat_mb,
-#                                         "y_hat_wb": y_hat_wb,
-#                                         "y_gt": y_gt,
-#                                         "total_loss": total_loss})
-#         optimizer = tf.keras.optimizers.Adam()
-#         loss_tracker = tf.keras.metrics.Mean(name="total_loss")
-#
-#         for i in range(10000):
-#             t0 = time.time()
-#             with tf.GradientTape() as tape:
-#                 outputs_dict = model([x1, mask_vector], training=True)
-#                 if (i + 1) % 100 == 0:
-#                     assert np.allclose(outputs_dict["norm_result_mb"].numpy(), outputs_dict["norm_result_wb"].numpy())
-#                 loss_tracker.update_state(values=outputs_dict["total_loss"])
-#             grads = tape.gradient(outputs_dict["total_loss"], model.trainable_variables)
-#             optimizer.apply_gradients(zip(grads, model.trainable_variables))
-#             t1 = time.time()
-#             print("{0} Loss:{1} Time:{2}".format(i, loss_tracker.result().numpy(), t1 - t0))
-#         results = model([x1, mask_vector], training=False)
-#
-#         print("x")
+if __name__ == "__main__":
+    print(tf.__version__)
+    gpus = tf.config.list_physical_devices('GPU')
+    tf.config.experimental.set_memory_growth(gpus[0], True)
+
+    with tf.device("GPU"):
+        batch_size = 125
+        dim = 128
+        momentum = 0.9
+        target_val = 5.0
+        x1 = np.random.uniform(low=-1.0, high=1.0, size=(batch_size, 32, 32, dim))
+        x2 = np.random.uniform(low=-1.0, high=1.0, size=(batch_size, 32, 32, dim))
+        mask_vector = np.random.randint(low=0, high=2, size=(batch_size,))
+
+        mb_norm = MaskedBatchNormalization(momentum=momentum)
+        wb_norm = WeightedBatchNormalization(momentum=momentum)
+
+        # func_str = tf.autograph.to_code(wb_norm.call.python_function)
+
+        i_x = tf.keras.Input(shape=(32, 32, dim))
+        mask_vector_tf = tf.keras.Input(shape=())
+        # net = tf.keras.layers.Dense(256)(i_x)
+        net = tf.keras.layers.Conv2D(filters=64,
+                                     kernel_size=3,
+                                     activation=tf.nn.relu,
+                                     strides=1,
+                                     padding="same",
+                                     use_bias=True,
+                                     name="conv")(i_x)
+        masked_net = tf.boolean_mask(net, mask_vector_tf)
+        norm_result_mb = mb_norm([net, masked_net])
+        norm_result_wb = wb_norm([net, mask_vector_tf])
+
+        mb_dense = tf.keras.layers.Flatten()(norm_result_mb)
+        wb_dense = tf.keras.layers.Flatten()(norm_result_wb)
+
+        y_hat_mb = tf.reduce_mean(mb_dense, axis=-1)
+        y_hat_wb = tf.reduce_mean(wb_dense, axis=-1)
+
+        y_gt = target_val * tf.ones_like(y_hat_mb)
+        reconstruction_loss_mb = tf.keras.losses.mean_squared_error(y_gt, y_hat_mb)
+        reconstruction_loss_wb = tf.keras.losses.mean_squared_error(y_gt, y_hat_wb)
+        total_loss = 0.5 * (reconstruction_loss_mb + reconstruction_loss_wb)
+        model = tf.keras.Model(inputs=[i_x, mask_vector_tf],
+                               outputs={"masked_net": masked_net,
+                                        "net": net,
+                                        "norm_result_mb": norm_result_mb,
+                                        "norm_result_wb": norm_result_wb,
+                                        "y_hat_mb": y_hat_mb,
+                                        "y_hat_wb": y_hat_wb,
+                                        "y_gt": y_gt,
+                                        "total_loss": total_loss})
+        optimizer = tf.keras.optimizers.Adam()
+        loss_tracker = tf.keras.metrics.Mean(name="total_loss")
+
+        for i in range(10000):
+            t0 = time.time()
+            with tf.GradientTape() as tape:
+                outputs_dict = model([x1, mask_vector], training=True)
+                if (i + 1) % 100 == 0:
+                    assert np.allclose(outputs_dict["norm_result_mb"].numpy(), outputs_dict["norm_result_wb"].numpy())
+                loss_tracker.update_state(values=outputs_dict["total_loss"])
+            grads = tape.gradient(outputs_dict["total_loss"], model.trainable_variables)
+            optimizer.apply_gradients(zip(grads, model.trainable_variables))
+            t1 = time.time()
+            print("{0} Loss:{1} Time:{2}".format(i, loss_tracker.result().numpy(), t1 - t0))
+        results = model([x1, mask_vector], training=False)
+
+        print("x")
