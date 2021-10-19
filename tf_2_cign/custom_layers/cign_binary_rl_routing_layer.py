6a25054599194cc7739a111dfded0b5f0c01101b
Ufuk <ufuk.bicici@bragi.com>
2021-10-19 13:53:14 +0300

    Binary RL: Binary routing layer implemented.

--- tf_2_cign/custom_layers/cign_binary_rl_routing_layer.py
+++ tf_2_cign/custom_layers/cign_binary_rl_routing_layer.py
@@ -9,13 +9,14 @@ class CignBinaryRlRoutingLayer(tf.keras.layers.Layer):
         self.level = level
         self.node = node
         self.network = network
+        self.nodeCountInThisLevel = len(self.network.orderedNodesPerLevel[level])
 
     def call(self, inputs, **kwargs):
         q_table_predicted = inputs[0]
         input_ig_routing_matrix = inputs[1]
         is_warm_up_period = inputs[2]
         ig_activations = inputs[3]
-        sc_routing_matrix = inputs[4]
+        sc_routing_matrix_prev_level = inputs[4]
         is_training = kwargs["training"]
         # q_table_predicted_cign_output, input_ig_routing_matrix, self.warmUpPeriodInput, ig_activations_array
 
@@ -38,61 +39,53 @@ class CignBinaryRlRoutingLayer(tf.keras.layers.Layer):
 
         sc_mask_vectors = [self.network.scMaskInputsDict[node.index]
                            for node in self.network.orderedNodesPerLevel[self.level]]
-        sc_routing_matrix_2 = tf.stack(sc_mask_vectors, axis=-1)
+        sc_routing_matrix_prev_level_2 = tf.stack(sc_mask_vectors, axis=-1)
 
         # Binary routing calculation algorithm:
         # If final_actions[i] == 0:
-        # We will look into sc_routing_matrix[i, :] for the "current level".
-        # sc_routing_matrix[i, :] contains the nodes which are activated in this level.
+        # We will look into sc_routing_matrix_prev_level[i, :] for the "current level".
+        # sc_routing_matrix_prev_level[i, :] contains the nodes which are activated in this level.
         # We will activate those nodes in the next layer such that:
         # 1) They are children of the nodes in the previous layer, which are indicated by the "1" entries in
-        # sc_routing_matrix[i, :].
+        # sc_routing_matrix_prev_level[i, :].
         # 2) They are the children of the nodes as indicated in "1)" but only if they are picked by the information
         # driven routing distribution in their parent nodes. Meaning i is picked if for its parent node j:
         # i = argmax_k p(n_j=k|x) for the current sample.
         # If final_actions[i] == 1:
-        # We will look into sc_routing_matrix[i, :] for the "current level".
+        # We will look into sc_routing_matrix_prev_level[i, :] for the "current level".
         # For every active node in this layer, we will activate "all" the children of that active node, in the
         # next layer.
 
+        # ig_activations = tf.stack(
+        #     [self.igActivationsDict[nd.index] for nd in self.orderedNodesPerLevel[level]], axis=-1)
+        # ************ final_actions[i] == 0 ************
+        sc_routing_matrix_action_0 = []
+        for nd_idx in range(self.nodeCountInThisLevel):
+            activations_nd = ig_activations[:, :, nd_idx]
+            sc_routing_vector = sc_routing_matrix_prev_level[:, nd_idx]
+            ig_indices = tf.argmax(activations_nd, axis=-1)
+            ig_routing_matrix = tf.one_hot(ig_indices, activations_nd.shape[1])
+            rl_routing_matrix = sc_routing_vector * ig_routing_matrix
+            sc_routing_matrix_action_0.append(rl_routing_matrix)
+        # ************ final_actions[i] == 0 ************
 
+        # ************ final_actions[i] == 1 ************
+        sc_routing_matrix_action_1 = []
+        for nd_idx in range(self.nodeCountInThisLevel):
+            sc_routing_vector = sc_routing_matrix_prev_level[:, nd_idx]
+            rl_routing_matrix = tf.ones_like(sc_routing_matrix_action_0[nd_idx])
+            rl_routing_matrix = sc_routing_vector * rl_routing_matrix
+            sc_routing_matrix_action_1.append(rl_routing_matrix)
+        # ************ final_actions[i] == 1 ************
 
-        print("X")
+        sc_routing_matrix_action_0 = tf.concat(sc_routing_matrix_action_0, axis=-1)
+        sc_routing_matrix_action_1 = tf.concat(sc_routing_matrix_action_1, axis=-1)
+        sc_routing_matrix = tf.where(final_actions, sc_routing_matrix_action_1, sc_routing_matrix_action_0)
 
+        # Warm up
+        sc_routing_matrix_warm_up = tf.ones_like(sc_routing_matrix)
 
-
-    # @tf.function
-    # def call(self, inputs, **kwargs):
-    #     q_table_predicted = inputs[0]
-    #     input_ig_routing_matrix = inputs[1]
-    #     is_warm_up_period = inputs[2]
-    #     past_actions = inputs[3]
-    #
-    #     # Get the feasibility table
-    #     feasibility_matrix = tf.gather_nd(self.reachabilityMatrices[self.level], tf.expand_dims(past_actions, axis=-1))
-    #     # Set entries for unfeasible actions to -inf, feasible actions to 0.
-    #     penalty_matrix = tf.where(tf.cast(feasibility_matrix, dtype=tf.bool),
-    #                               0.0,
-    #                               CignRlRoutingLayer.infeasible_action_penalty)
-    #     # Final q table, with Bellman Optimality.
-    #     q_table_with_penalties = penalty_matrix + q_table_predicted
-    #     predicted_actions = tf.argmax(q_table_with_penalties, axis=-1)
-    #     # secondary_routing_matrix = self.actionSpaces[self.level][predicted_actions]
-    #     secondary_routing_matrix = tf.gather_nd(self.actionSpaces[self.level],
-    #                                             tf.expand_dims(predicted_actions, axis=-1))
-    #     secondary_routing_matrix_logical_or_ig = tf.cast(
-    #         tf.logical_or(tf.cast(secondary_routing_matrix, dtype=tf.bool),
-    #                       tf.cast(input_ig_routing_matrix, dtype=tf.bool)), dtype=tf.int32)
-    #
-    #     # Actions are updated with the inclusion of information gain.
-    #     if self.useIgInActions:
-    #         basis_matrix = secondary_routing_matrix_logical_or_ig * \
-    #                        tf.expand_dims(self.network.actionSpacesReverse[self.level], axis=0)
-    #         predicted_actions = tf.reduce_sum(basis_matrix, axis=-1) - 1
-    #
-    #     secondary_routing_matrix_warm_up = tf.ones_like(secondary_routing_matrix_logical_or_ig)
-    #
-    #     secondary_routing_matrix_final = tf.where(tf.cast(is_warm_up_period, tf.int32) > 0,
-    #                                               secondary_routing_matrix_warm_up,
-    #                                               secondary_routing_matrix_logical_or_ig)
-    #     return predicted_actions, secondary_routing_matrix_final
+        sc_routing_matrix_final = tf.where(tf.cast(is_warm_up_period, tf.int32) > 0,
+                                           sc_routing_matrix_warm_up,
+                                           sc_routing_matrix)
+        return final_actions, sc_routing_matrix_final
