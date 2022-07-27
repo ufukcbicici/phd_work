e22132d891f8c5ad215a92c1bcabf745ef69db40
Ufuk <ufuk.bicici@bragi.com>
2022-07-27 11:02:34 +0300

    Multipath capacity has been tested. Looks good!

--- tf_2_cign/cigt/data_classes/multipath_routing_info.py
+++ tf_2_cign/cigt/data_classes/multipath_routing_info.py
@@ -1,6 +1,7 @@
 import numpy as np
 import tensorflow as tf
 from tqdm import tqdm
+import time
 from dataclasses import dataclass
 from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 
@@ -161,11 +162,30 @@ class MultipathCombinationInfo(object):
             # routing decisions.
 
             # 1) Get the entropy of each sample, based on the past decisions.
+            t0 = time.time()
             curr_sample_entropies = np.apply_along_axis(func1d=lambda row: self.past_decisions_entropies_dict[
                 tuple(row[:past_num_of_routes])][row[-1]], arr=sample_paths, axis=1)
             # 2) Find the relevant entropy intervals for each sample
-
-
+            t1 = time.time()
+            entropy_intervals = list_of_entropy_intervals[block_id]
+            interval_selections = \
+                np.less_equal(np.expand_dims(curr_sample_entropies, axis=-1), entropy_intervals)
+            interval_ids = np.argmax(interval_selections, axis=1)
+            # 3) Get the probability thresholds with respect to the selected entropy intervals.
+            t2 = time.time()
+            probability_thresholds = list_of_probability_thresholds[block_id]
+            probability_thresholds_per_sample = probability_thresholds[interval_ids]
+            # 4) Get the current routing probabilities of each sample, based on the paths taken so far.
+            t3 = time.time()
+            curr_sample_routing_probabilities = np.apply_along_axis(
+                func1d=lambda row: self.past_decisions_routing_probabilities_dict[
+                    tuple(row[:past_num_of_routes])][row[-1]], arr=sample_paths, axis=1)
+            assert curr_sample_routing_probabilities.shape == probability_thresholds_per_sample.shape
+            # 5) Compare current routing probabilities to the thresholds, integrate new path selections
+            t4 = time.time()
+            route_selections = np.greater_equal(curr_sample_routing_probabilities,
+                                                probability_thresholds_per_sample).astype(sample_paths.dtype)
+            sample_paths = np.concatenate([sample_paths[:, :-1], route_selections, indices], axis=1)
             print("X")
             past_num_of_routes += route_count
 
