ab74b9ea28469643fd89f2c9029488d07c68be94
Ufuk <ufuk.bicici@bragi.com>
2022-08-22 00:59:40 +0300

    Refactoring Cross Entropy Search based threshold optimization into a separate structure.

--- tf_2_cign/cigt/cross_entropy_optimizers/cross_entropy_threshold_optimizer.py
+++ tf_2_cign/cigt/cross_entropy_optimizers/cross_entropy_threshold_optimizer.py
@@ -1,5 +1,8 @@
+import numpy as np
 import tensorflow as tf
 import os
+
+from sklearn.model_selection import train_test_split
 
 from auxillary.db_logger import DbLogger
 from auxillary.parameters import DiscreteParameter
@@ -24,6 +27,7 @@ class CrossEntropySearchOptimizer(object):
         self.pathCounts = list(self.model.pathCounts)
         self.multiPathInfoObject = MultipathCombinationInfo(batch_size=self.model.batchSize,
                                                             path_counts=self.pathCounts)
+        self.totalSampleCount, self.valIndices, self.testIndices = None, None, None
 
     # Load routing information for the particular model
     def load_model_data(self):
@@ -31,6 +35,27 @@ class CrossEntropySearchOptimizer(object):
                                                        dataset=self.dataset.testDataTf)
         self.multiPathInfoObject.assert_routing_validity(cigt=self.model)
         self.multiPathInfoObject.assess_accuracy()
+        self.totalSampleCount, self.valIndices, self.testIndices = self.prepare_val_test_sets()
+
+    def prepare_val_test_sets(self):
+        total_sample_count = set()
+        for ll in self.multiPathInfoObject.combinations_routing_probabilities_dict.values():
+            for arr in ll:
+                total_sample_count.add(arr.shape[0])
+        for ll in self.multiPathInfoObject.combinations_routing_entropies_dict.values():
+            for arr in ll:
+                total_sample_count.add(arr.shape[0])
+        for arr in self.multiPathInfoObject.combinations_y_hat_dict.values():
+            total_sample_count.add(arr.shape[0])
+        for arr in self.multiPathInfoObject.combinations_y_dict.values():
+            total_sample_count.add(arr.shape[0])
+        assert len(total_sample_count) == 1
+        total_sample_count = list(total_sample_count)[0]
+        val_sample_count = int(total_sample_count * self.valRatio)
+        indices = np.arange(total_sample_count)
+        val_indices, test_indices = train_test_split(indices, train_size=val_sample_count)
+        return total_sample_count, val_indices, test_indices
+
 
 
 
